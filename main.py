02c41c04842b3bb0c3887ae02aae3de164a5784e
TommasoBambagiotti <61661628+TommasoBambagiotti@users.noreply.github.com>
2022-10-06 19:04:14 +0200

    added correlation functions and log derivatives

--- main.py
+++ main.py
@@ -8,14 +8,19 @@ from numpy.polynomial import hermite
 
 
 def anharmonic_oscillator_diag(f, n_dim, w0):
+
+    #Units of measurement: h_bar=1, m=1/2, lambda=1
+
     # parameters
     n_max = n_dim + 4
+    #n_max = 100
     eps = pow(1, -30)
 
     # euclidean time coordinate
     tau_max = 2.5
     n_tau = 100
     dtau = tau_max / n_tau
+
 
     tau_axis = np.arange(0, tau_max, dtau)
 
@@ -50,6 +55,10 @@ def anharmonic_oscillator_diag(f, n_dim, w0):
     corr_funct2 = np.zeros(n_tau)
     corr_funct3 = np.zeros(n_tau)
 
+    derivative_log_corr_funct = np.zeros(n_tau)
+    derivative_log_corr_funct2 = np.zeros(n_tau)
+    derivative_log_corr_funct3 = np.zeros(n_tau)
+
     # mass???
 
     m = 0.5
@@ -65,7 +74,7 @@ def anharmonic_oscillator_diag(f, n_dim, w0):
     c44 = pow(c_w0, 4) / 4
     c68 = pow(c_w0, 6) / 8
 
-    #### Hamiltonian, symmetric n_dim x n_dim matrix ####
+    # Hamiltonian, symmetric n_dim x n_dim matrix
 
     for n in range(n_dim):
         # n=k-1
@@ -90,16 +99,22 @@ def anharmonic_oscillator_diag(f, n_dim, w0):
         h[n, n + 4] = x4
         h[n + 4, n] = x4
 
-    ### Diagononilzation of the Hamiltonian ###
+    # Diagononilzation of the Hamiltonian
     e, v = LA.eigh(h)
 
-    # WE consider also the positive eigenvalues: for some reason the first 4 e are < 0. So we neglect the first 4 eigenvectors and consider the groundstate v[:,4]=> because the eigenvectors are the columns
+    # Eigenvalues and Eigenvectors check
+    #print(e)
+
+    # WE consider also the positive eigenvalues: for some reason the first 4 e are < 0.
+    # So we neglect the first 4 eigenvectors and consider the groundstate v[:,4]=> because the eigenvectors are the columns.
     e = np.delete(e, [0, 1, 2, 3])
     v = np.delete(v, [0, 1, 2, 3], 1)  # we eliminate the first 4 columns
 
-    ### Evaluate the energy dist. rho, rho2, rho2 and matrix elements < 0|x^i|n > , i= 1,2,3 ###
-    ### Control the results and the cycle indices
-    ### We use the convenction: a+ = (mw/2h)^1/2 (x^ +i/mw p^) and a = (mw/2h)^1/2 (x^ - i/mw p^)
+    print(e)
+
+    # Evaluate the energy dist. rho, rho2, rho2 and matrix elements < 0|x^i|n > , i= 1,2,3
+    # Control the results and the cycle indices
+    # We use the convenction for the ladder operators: a+ = (mw/2h)^1/2 (x^ +i/mw p^) and a = (mw/2h)^1/2 (x^ - i/mw p^)
     for n in range(n_dim):
         # C_n = | <0|x|n> |^2
         # D_n = | <0|x^2|n> |^2
@@ -127,17 +142,17 @@ def anharmonic_oscillator_diag(f, n_dim, w0):
                                                                                        1 / 2) * v[k_plus_3, 0]) * v[
                        k, n]
 
-        ###
+
 
         rho[n] = c22 * pow(C_n, 2)
         rho2[n] = c44 * pow(D_n, 2)
         rho3[n] = c68 * pow(E_n, 2)
 
-    ### Groundstate wave function and its properties ###
+    # Groundstate wave function and its properties
 
     x = 0.0
 
-    ### Definition of a basis of Hermite pol. ###
+    # Definition of a basis of Hermite pol.
 
     Hermite_coeff = np.zeros((n_dim))
 
@@ -154,8 +169,8 @@ def anharmonic_oscillator_diag(f, n_dim, w0):
         val = 1.0
 
         for n in range(n_dim):
-            ### follow the formula for the Eig state of the harmonic osc. : 1/(l^1/2 pi^1/4 (2^n * n!)^1/2) * exp(-x^2/l^2 /2) * H_n(x/l)
-
+            # follow the formula for the Eig state of the harmonic osc. : 1/(l^1/2 pi^1/4 (2^n * n!)^1/2) * exp(-x^2/l^2 /2) * H_n(x/l)
+            # length scale of the harmonic oscillator: l=c=(1/mw0)^1/2
             val = pow(np.pi * c_w0 * c_w0, -1 / 4)
 
             val *= pow(2.0, -n / 2)  # 1^ normalization
@@ -173,18 +188,18 @@ def anharmonic_oscillator_diag(f, n_dim, w0):
             val = 1.0
 
             Psi_x += v[n, 0] * psi[n]  # * termine corr[n]
-        ###
+
 
         Psi_X2[k] = Psi_x * Psi_x
 
-        ### Compare to the simple model ###
+        # Compare to the simple model
         Psi_p = pow((2.0 * f / np.pi), 1 / 4) * np.exp(-f * pow(x - f, 2))
         Psi_m = pow((2.0 * f / np.pi), 1 / 4) * np.exp(-f * pow(x + f, 2))
         Psi_0 = (Psi_p + Psi_m) * pow(2, -1 / 2)
 
         Psi0_ar[k] = Psi_0 * Psi_0
 
-        ### Check Normalization ###
+        # Check Normalization
         xnorm1 += dx * pow(Psi_x, 2)
 
     # xnorm = np.trapz(Psi_X2, x_axis, dx)
@@ -193,7 +208,7 @@ def anharmonic_oscillator_diag(f, n_dim, w0):
     # print('xnorm1 for Psi_x in dim = {first}'.format(first=n_dim))
     # print(xnorm1)
 
-    ### Map potential and Psi_x vs. (Psi+ + Psi-)
+    # Map potential and Psi_x vs. (Psi+ + Psi-)
     Fig1 = plt.figure(1)
 
     V_x = np.zeros((n_x))
@@ -210,15 +225,15 @@ def anharmonic_oscillator_diag(f, n_dim, w0):
 
     plt.plot(x_axis, Psi_X2, 'b', x_axis, Psi0_ar, '-r')
 
-    ### Coordinate space correlator ###
+    # Coordinate space correlator
 
-    ### Groundstate eigenvalue ###
+    # Groundstate eigenvalue
 
     E_0 = e[0]
 
-    ### Temporal coord. ###
+    # Temporal coord.
     tau = 0.0
-    ### temporary var ###
+    # temporary var
     xCor_temp1 = 0.0
     xCor_temp2 = 0.0
     xCor_temp3 = 0.0
@@ -238,11 +253,82 @@ def anharmonic_oscillator_diag(f, n_dim, w0):
         xCor_temp2 = 0.0
         xCor_temp3 = 0.0
 
+
+
     Fig3 = plt.figure(3)
 
     plt.plot(tau_axis, corr_funct, 'b', linestyle='--')
     plt.plot(tau_axis, corr_funct2, color='r', linestyle='-')
     plt.plot(tau_axis, corr_funct3, color='g', linestyle='dashdot')
+    plt.show()
+
+    # Logarithmic derivative of the correlators
+
+    # Python methods
+    py_derivative_log_corr_funct = np.gradient(-corr_funct,dtau)
+    py_derivative_log_corr_funct2 = np.gradient(-corr_funct2, dtau)
+    py_derivative_log_corr_funct3 = np.gradient(-corr_funct3, dtau)
+
+    plt.plot(tau_axis,py_derivative_log_corr_funct,color='blue', linestyle='--')
+    plt.plot(tau_axis, py_derivative_log_corr_funct2, color='red',linestyle='-')
+    plt.plot(tau_axis, py_derivative_log_corr_funct3, color='green', linestyle='dashdot')
+    plt.axis([0, 1.5, 0, 8]) # plt.axis([xmin,xmax,ymin,ymax] set axes limits
+    plt.title('d/dt[log<O(0)O(t)>] - Numpy')
+    plt.show()
+
+    # Forward difference
+
+    fd_derivative_log_corr_funct = np.zeros(n_tau) #da spostare all'inizio del codice
+    fd_derivative_log_corr_funct2 = np.zeros(n_tau)
+    fd_derivative_log_corr_funct3 = np.zeros(n_tau)
+
+    for i in range(n_tau-1):
+        fd_derivative_log_corr_funct[i] = - (np.log(corr_funct[i+1])-np.log(corr_funct[i]))/dtau
+        fd_derivative_log_corr_funct2[i] = - (np.log(corr_funct2[i + 1]) - np.log(corr_funct2[i])) / dtau
+        fd_derivative_log_corr_funct3[i] = - (np.log(corr_funct3[i + 1]) - np.log(corr_funct3[i])) / dtau
+
+    plt.plot(tau_axis,fd_derivative_log_corr_funct,color='blue', linestyle='--')
+    plt.plot(tau_axis, fd_derivative_log_corr_funct2, color='red',linestyle='-')
+    plt.plot(tau_axis, fd_derivative_log_corr_funct3, color='green', linestyle='dashdot')
+    plt.axis([0, 1.5, 0, 8]) # plt.axis([xmin,xmax,ymin,ymax] set axes limits
+    plt.title('d/dt[log<O(0)O(t)>] - Forward difference')
+    plt.show()
+
+    # Analytic formula
+
+    # Time variable
+    tau = 0.0
+
+    # Temporary variables
+    derivative_xCor_temp1 = 0.0
+    derivative_xCor_temp2 = 0.0
+    derivative_xCor_temp3 = 0.0
+
+    for i in range(n_tau):
+
+        tau = i*dtau
+
+        for j in range(n_dim):
+
+            derivative_xCor_temp1 = derivative_xCor_temp1 + rho[j] * (e[j] - E_0) * np.exp(-(e[j] - E_0) * tau)
+            derivative_xCor_temp2 = derivative_xCor_temp2 + rho2[j] * (e[j] - E_0) * np.exp(-(e[j] - E_0) * tau)
+            derivative_xCor_temp3 = derivative_xCor_temp3 + rho3[j] * (e[j] - E_0) * np.exp(-(e[j] - E_0) * tau)
+
+        #Derivatives of the log of the correlation functions
+        derivative_log_corr_funct[i] = derivative_xCor_temp1 / corr_funct[i]
+        derivative_log_corr_funct2[i] = derivative_xCor_temp2 / (corr_funct2[i] - rho2[0]) #subtract the offset |<0|x^2|0>|^2 to have the right convergence
+        derivative_log_corr_funct3[i] = derivative_xCor_temp3 / corr_funct3[i]
+
+        derivative_xCor_temp1 = 0.0
+        derivative_xCor_temp2 = 0.0
+        derivative_xCor_temp3 = 0.0
+
+
+    plt.plot(tau_axis,derivative_log_corr_funct,color='blue', linestyle='--')
+    plt.plot(tau_axis, derivative_log_corr_funct2, color='red',linestyle='-')
+    plt.plot(tau_axis, derivative_log_corr_funct3, color='green', linestyle='dashdot')
+    plt.axis([0, 1.5, 0, 8]) # plt.axis([xmin,xmax,ymin,ymax] set axes limits
+    plt.title('d/dt[log<O(0)O(t)>] - Analytic formula')
     plt.show()
 
 
