379bfb9b1c0b512e359acd5309856ec51c11f016
Federt1 <fedeale98.fa@gmail.com>
2022-10-08 15:38:41 +0200

    Added some corrections to the file
    Ho ridefinito alcune variabili e cercato di mettere un po' in ordine il tutto.
    Ho notato che prima la cosa che rallentava il tutto era creare l'array delle posizioni per l'istogramma. Facendo l'append ogni volta si creava un array enorme che dovevi modificare ogni ciclo. Per provare ho fatto in modo di scrivere i valori in un file che poi viene riletto in un secondo momento. Naturalmente sono tutte prove, anche perché sono cose che non conosco bene.

--- mnt_crl_ao.py
+++ mnt_crl_ao.py
@@ -1,309 +1,291 @@
-import matplotlib as mpl
 
+import struct
+import time
+import random as rnd
+import numpy as np
 import matplotlib.pyplot as plt
 
-import numpy as np
 
-import random as rnd
-
-import time
-
-# Solve the anharmonic oscillator through Monte Carlo technique on an Euclidian Axis 
-
-def Monte_Carlo_AO (f,nE_lattice, dtau, nEquil, nMCsweeps, delta_x, nxCorr, nMeas, nPri,icold):
-
-    if(nMCsweeps < nEquil):
+def monte_carlo_ao(x_potential_minimum,
+                   n_lattice,
+                   dtau,
+                   n_equil,
+                   n_mc_sweeps,
+                   delta_x,
+                   n_point,
+                   n_meas,
+                   n_print,
+                   icold):
+    '''Solve the anharmonic oscillator through Monte Carlo technique on an Euclidian Axis'''
+    if n_mc_sweeps < n_equil:
         print("too few Monte Carlo sweeps/ N_equilib > N_Monte_Carlo")
 
-    # tau spacing
-    #tau_max = nE_lattice* dtau
-
-    tau_axis = np.arange(0,dtau*nE_lattice,dtau)
-
     # Correlation
+    x_cor_sum = np.zeros((n_point))
+    x_cor_2_sum = np.zeros((n_point))
+    x_cor_3_sum = np.zeros((n_point))
 
-    xCor_sum = np.zeros((nxCorr))
-    xCor_2_sum = np.zeros((nxCorr))
-    xCor_3_sum = np.zeros((nxCorr))
-
-    x2Cor_sum = np.zeros((nxCorr))
-    x2Cor_2_sum = np.zeros((nxCorr))
-    x2Cor_3_sum = np.zeros((nxCorr))
+    x2_cor_sum = np.zeros((n_point))
+    x2_cor_2_sum = np.zeros((n_point))
+    x2_cor_3_sum = np.zeros((n_point))
 
     # Averages and errors
 
-    xCor_av = np.zeros((nxCorr))
-    xCor_err = np.zeros((nxCorr))
+    x_cor_av = np.zeros((n_point))
+    x_cor_err = np.zeros((n_point))
 
-    xCor_2_av = np.zeros((nxCorr))
-    xCor_2_err = np.zeros((nxCorr))
+    x_cor_2_av = np.zeros((n_point))
+    x_cor_2_err = np.zeros((n_point))
 
-    xCor_3_av = np.zeros((nxCorr))
-    xCor_3_err = np.zeros((nxCorr))
-
+    x_cor_3_av = np.zeros((n_point))
+    x_cor_3_err = np.zeros((n_point))
 
     # x position along the tau axis
-    x_Config = np.zeros((nE_lattice+1))
+    x_config = np.zeros((n_lattice+1))
 
     #histogram for the wave function
 
-    x_nHist = 100
-    x_Config_hist = np.empty((nE_lattice),float)
-    x_Config_hist_temp = np.empty((nE_lattice),float)
+    n_bin_hist = 100
 
     # initialize the lattice
 
-    rnd_Uniform = rnd.Random(time.time())
+    rnd_uniform = rnd.Random(time.time())
+    rnd_gauss = rnd.Random(time.time())
 
-    rnd_Gauss = rnd.Random(time.time())
-
-
-    if(icold == True):
-        for i in range(nE_lattice):
-            x_Config[i] = -f
-
+    if icold is True:
+        for i in range(n_lattice):
+            x_config[i] = -x_potential_minimum
     else:
         rnd.seed()
-        for i in range(nE_lattice):
-            x_Config[i] = 2*rnd_Uniform.uniform(0.,1.)*f -f
+        for i in range(n_lattice):
+            x_config[i] = 2 * \
+                rnd_uniform.uniform(0., 1.)*x_potential_minimum - x_potential_minimum
 
-    # impose periodic boundary conditions and understand the way in which they are imposed in the reference file
-    x_Config[nE_lattice-1] = x_Config[0]
-    x_Config[nE_lattice] = x_Config[1]
-
-    #plt.plot(tau_axis, x_axis)
-    #plt.show()
-
-    # Evaluate the initial action
-
-    S_initial = 0.0
-
-    for i in range(nE_lattice-1):
-        T = pow((x_Config[i+1] - x_Config[i])/(2*dtau),2)
-        V = pow(x_Config[i]* x_Config[i]-f*f,2)
-        S_initial += (T+V)*dtau
-
-    print(S_initial)
+    # impose periodic boundary conditions and
+    # understand the way in which they are imposed in the reference file
+    x_config[n_lattice-1] = x_config[0]
+    x_config[n_lattice] = x_config[1]
 
     # Monte Carlo sweeps: Principal cycle
 
-    nAccept = 0
-    nHit = 0
-    nConfig = 0
-    nCorr = 0
+    # Counters for different parts
+    n_accept = 0
+    n_hit = 0
+    n_config = 0
+    n_corr = 0
 
-    # Local exp(-S) for each point x_k
+    # Action, Kin en and Potential en
+    action_total = 0.0  # total action
+    kin_en_total = 0.0
+    pot_en_total = 0.0
 
+    # Temporary variables
+    kin_en_temp = 0.0
+    pot_en_temp = 0.0
 
-    S_total = 0.0 # total action
-    T = 0.0
-    V = 0.0
+    # Output file hist
+    with open('hist.dat', 'wb') as writer:
 
+        # Equilibration cycle
+        while n_config < n_equil:
+            for i in range(1, n_lattice):
+                #we apply Metropolis algorithm for each site tau_k:
+                #Secundary cycle
+                n_hit += 1
 
-    # Equilibrization cycle
+                action_loc_old = (
+                    pow((x_config[i] - x_config[i-1])/(2*dtau), 2) +
+                    pow((x_config[i+1] - x_config[i])/(2*dtau), 2) +
+                    pow(x_config[i] * x_config[i] -
+                        x_potential_minimum*x_potential_minimum, 2)
+                )*dtau
 
-    for k in range (nEquil):
+                x_new = x_config[i] + rnd_gauss.gauss(0, delta_x)
 
-        for i in range(1,nE_lattice): #we apply Metropolis algorithm for each site tau_k: Secundary cycle
+                action_loc_new = (
+                    pow((x_new - x_config[i-1])/(2*dtau), 2) +
+                    pow((x_config[i+1] - x_new)/(2*dtau), 2) +
+                    pow(x_new * x_new-x_potential_minimum*x_potential_minimum, 2)
+                )*dtau
 
-            nHit += 1
+                delta_action = action_loc_new - action_loc_old
 
-            t0m = pow((x_Config[i] - x_Config[i-1])/(2*dtau),2)
-            t0p = pow((x_Config[i+1] - x_Config[i])/(2*dtau),2)
-            v = pow(x_Config[i]* x_Config[i]-f*f,2)
+                # we put a bound on the value of delta_S because we need the exp.
+                delta_action = max(delta_action, -70.0)
+                delta_action = min(delta_action, 70.0)
+                # Metropolis question:
+                if np.exp(-delta_action) > rnd_uniform.uniform(0., 1.):
+                    x_config[i] = x_new
+                    n_accept += 1
 
-            sLoc_now = (t0m + t0p + V)*dtau
+            x_config[0] = x_config[n_lattice-1]
+            x_config[n_lattice] = x_config[1]
 
-            x_New = x_Config[i] + rnd_Gauss.gauss(0,delta_x)
+            n_config += 1
 
-            t0m = pow((x_New - x_Config[i-1])/(2*dtau),2)
-            t0p = pow((x_Config[i+1] - x_New)/(2*dtau),2)
-            v = pow(x_New* x_New-f*f,2)
+        n_config = 0
 
-            sLoc_new = (t0m + t0p + v)*dtau
+        #Rest of the MC sweeps
+        while (n_config + n_equil) < n_mc_sweeps:
+            for i in range(1, n_lattice):
+                n_hit += 1
 
-            delta_sLoc = sLoc_new - sLoc_now
+                action_loc_old = (
+                    pow((x_config[i] - x_config[i-1])/(2*dtau), 2) +
+                    pow((x_config[i+1] - x_config[i])/(2*dtau), 2) +
+                    pow(x_config[i] * x_config[i] -
+                        x_potential_minimum*x_potential_minimum, 2)
+                )*dtau
 
-            P = rnd_Uniform.uniform(0.,1.)
+                x_new = x_config[i] + rnd_gauss.gauss(0, delta_x)
 
-            # we put a bound on the value of delta_S because we need the exp.
-            delta_sLoc = max(delta_sLoc,-70.0)
-            delta_sLoc = min(delta_sLoc,70.0)
-            # Metropolis question:
-            if( np.exp(-delta_sLoc) > P):
-                x_Config[i] = x_New
-                nAccept += 1
+                action_loc_new = (
+                    pow((x_new - x_config[i-1])/(2*dtau), 2) +
+                    pow((x_config[i+1] - x_new)/(2*dtau), 2) +
+                    pow(x_new * x_new-x_potential_minimum*x_potential_minimum, 2)
+                )*dtau
 
+                delta_action = action_loc_new - action_loc_old
 
-        x_Config[0] = x_Config[nE_lattice-1]
-        x_Config[nE_lattice] = x_Config[1]
+                # we put a bound on the value of delta_S because we need the exp.
+                delta_action = max(delta_action, -70.0)
+                delta_action = min(delta_action, 70.0)
+                # Metropolis question:
+                if np.exp(-delta_action) > rnd_uniform.uniform(0., 1.):
+                    x_config[i] = x_new
+                    n_accept += 1
 
-        # Calculate the total action:
+                writer.write(x_config[i])
 
-        for j in range (nE_lattice-1):
-            T = pow((x_Config[j+1] - x_Config[j])/(2*dtau),2)
-            V = pow(x_Config[j]* x_Config[j]-f*f,2)
-            S_total += (T+V)*dtau
+            x_config[0] = x_config[n_lattice-1]
 
-        S_total = 0.0
+            writer.write(x_config[0])
 
-    #Rest of the MC sweeps
+            x_config[n_lattice] = x_config[1]
 
-    for k in range(nEquil, nMCsweeps):
+            # Calculate the total action:
+            for j in range(n_lattice-1):
+                kin_en_temp = pow((x_config[j+1] - x_config[j])/(2*dtau), 2)
+                pot_en_temp = pow(
+                    x_config[j] * x_config[j]-x_potential_minimum*x_potential_minimum, 2)
 
-        nConfig += 1
+                kin_en_total += kin_en_temp
+                pot_en_total += pot_en_temp
+                action_total += (kin_en_temp + pot_en_temp)*dtau
 
-        for i in range(1,nE_lattice): #we apply Metropolis algorithm for each site tau_k: Secundary cycle
+                kin_en_temp = 0.0
+                pot_en_temp = 0.0
 
-            nHit += 1
+            # output control at each n_print step
 
-            t0m = pow((x_Config[i] - x_Config[i-1])/(2*dtau),2)
-            t0p = pow((x_Config[i+1] - x_Config[i])/(2*dtau),2)
-            v = pow(x_Config[i]* x_Config[i]-f*f,2)
+            #if( (k % n_print) == 0 ):
+                #print('acceptance ratio = {n}'.format(n= n_accept/n_hit))
 
-            sLoc_now = (t0m + t0p + V)*dtau
+            for k_meas in range(n_meas):
+                n_corr += 1
+                
+                i_p0 = int((n_lattice - n_point) * rnd_uniform.uniform(0., 1.))
+                x_0 = x_config[i_p0]
+                for i_corr in range(n_point):
+                    x_1 = x_config[i_p0 + i_corr]
 
-            x_New = x_Config[i] + rnd_Gauss.gauss(0,delta_x)
+                    x_cor_sum[i_corr] += x_0 * x_1
+                    x_cor_2_sum[i_corr] += pow(x_0 * x_1, 2)
+                    x_cor_3_sum[i_corr] += pow(x_0 * x_1, 3)
 
-            t0m = pow((x_New - x_Config[i-1])/(2*dtau),2)
-            t0p = pow((x_Config[i+1] - x_New)/(2*dtau),2)
-            v = pow(x_New* x_New-f*f,2)
+                    x2_cor_sum[i_corr] += pow(x_0 * x_1, 2)
+                    x2_cor_2_sum[i_corr] += pow(x_0 * x_1, 4)
+                    x2_cor_3_sum[i_corr] += pow(x_0 * x_1, 6)
+                    # Close the output file
 
-            sLoc_new = (t0m + t0p + v)*dtau
+            n_config += 1
 
-            delta_sLoc = sLoc_new - sLoc_now
-
-            P = rnd_Uniform.uniform(0.,1.)
-
-            # we put a bound on the value of delta_S because we need the exp.
-            delta_sLoc = max(delta_sLoc,-70.0)
-            delta_sLoc = min(delta_sLoc,70.0)
-            # Metropolis question:
-            if( np.exp(-delta_sLoc) > P):
-                x_Config[i] = x_New
-                nAccept += 1
-
-        x_Config[0] = x_Config[nE_lattice-1]
-        x_Config[nE_lattice] = x_Config[1]
-
-
-        x_Config_hist_temp = x_Config_hist
-
-        x_Config_hist = np.append(x_Config_hist_temp, x_Config[0:(nE_lattice-1)])
-
-        # Calculate the total action:
-
-        for j in range (nE_lattice-1):
-            T = pow((x_Config[j+1] - x_Config[j])/(2*dtau),2)
-            V = pow(x_Config[j]* x_Config[j]-f*f,2)
-            S_total += (T+V)*dtau
-
-
-        # output control at each nPri step
-
-        #if( (k % nPri) == 0 ):
-            #print('acceptance ratio = {n}'.format(n= nAccept/nHit))
-
-        S_total = 0.0
-
-        for i_meas in range (nMeas):
-
-            nCorr += 1
-
-            i_p0 = int( (nE_lattice - nxCorr) * rnd_Uniform.uniform(0.,1.) )
-
-            x_0 = x_Config[i_p0]
-
-            for i_Corr in range (nxCorr):
-
-                x_1 = x_Config[i_p0 + i_Corr]
-
-                #x_1 = x_Config[i_Corr]
-
-                xCor = x_0 * x_1
-                xCor_2 = pow(xCor,2)
-                xCor_3 = pow(xCor,3)
-
-                xCor_sum[i_Corr] += xCor
-                xCor_2_sum[i_Corr] += xCor_2
-                xCor_3_sum[i_Corr] += xCor_3
-
-                x2Cor_sum[i_Corr] += pow(xCor,2)
-                x2Cor_2_sum[i_Corr] += pow(xCor_2,2)
-                x2Cor_3_sum[i_Corr] += pow(xCor_3,2)
-
-
-# Monte Carlo End
-
+    # Monte Carlo End
     #Evaluate averages and other stuff, maybe we can create a function
+    var = 0.0
+    var2 = 0.0
+    var3 = 0.0
 
-    Var = 0.0
-    Var2 = 0.0
-    Var3 = 0.0
+    for i_corr in range(n_point):
+        x_cor_av[i_corr] = x_cor_sum[i_corr]/n_corr
+        var = x2_cor_sum[i_corr]/(n_corr*n_corr) - \
+            pow(x_cor_av[i_corr], 2)/n_corr
 
-    for i_Corr in range (nxCorr):
-        xCor_av[i_Corr] = xCor_sum[i_Corr]/nCorr
-        Var = x2Cor_sum[i_Corr]/(nCorr*nCorr) - pow(xCor_av[i_Corr],2)/nCorr
+        x_cor_2_av[i_corr] = x_cor_2_sum[i_corr]/n_corr
+        var2 = x2_cor_2_sum[i_corr] / (n_corr*n_corr) - \
+            pow(x_cor_2_av[i_corr], 2)/n_corr
 
-        xCor_2_av[i_Corr] = xCor_2_sum[i_Corr]/nCorr
-        Var2 = x2Cor_2_sum[i_Corr]/(nCorr*nCorr) - pow(xCor_2_av[i_Corr],2)/nCorr
+        x_cor_3_av[i_corr] = x_cor_3_sum[i_corr]/n_corr
+        var3 = x2_cor_3_sum[i_corr] / (n_corr*n_corr) - \
+            pow(x_cor_3_av[i_corr], 2)/n_corr
 
-        xCor_3_av[i_Corr] = xCor_3_sum[i_Corr]/nCorr
-        Var3 = x2Cor_3_sum[i_Corr]/(nCorr*nCorr) - pow(xCor_3_av[i_Corr],2)/nCorr
-
-        if(Var > 0.):
-            xCor_err[i_Corr] = pow(Var,1/2)
+        if var > 0.0:
+            x_cor_err[i_corr] = pow(var, 1/2)
         else:
-            xCor_err[i_Corr] = 0.
+            x_cor_err[i_corr] = 0.
 
-        if(Var2 > 0.0):
-            xCor_2_err[i_Corr] = pow(Var2,1/2)
+        if var2 > 0.0:
+            x_cor_2_err[i_corr] = pow(var2, 1/2)
         else:
-            xCor_2_err[i_Corr] = 0.
+            x_cor_2_err[i_corr] = 0.
 
-        if(Var3 > 0.0):
-            xCor_3_err[i_Corr] = pow(Var3,1/2)
+        if var3 > 0.0:
+            x_cor_3_err[i_corr] = pow(var3, 1/2)
         else:
-            xCor_3_err[i_Corr] = 0.
+            x_cor_3_err[i_corr] = 0.
 
-    #Correlation function
-
-
-    tau_corr_axis = np.arange(0,nxCorr*dtau,dtau)
-
-
-    print('acceptance ratio = {n}'.format(n= nAccept/nHit))
+    #Control Acceptance ratio
+    print('acceptance ratio = {n}'.format(n=n_accept/n_hit))
 
     # In order for matplotlib to print text in LaTeX
     plt.rcParams.update({"text.usetex": True, "font.family": "Helvetica"})
 
-    Fig1 = plt.figure(1, figsize=(8, 2.5), facecolor="#f1f1f1")
-    left, bottom, width, height = 0.1, 0.1, 0.8, 0.8
-    ax1 = Fig1.add_axes((left,bottom,width,height), facecolor="#e1e1e1")
+    #Print correlation function plot
+    tau_corr_axis = np.arange(0, n_point*dtau, dtau)
+    fig1 = plt.figure(1, figsize=(8, 2.5), facecolor="#f1f1f1")
+    ax1 = fig1.add_axes((0.1, 0.1, 0.8, 0.8), facecolor="#e1e1e1")
 
     ax1.set_xlabel(r"$\tau$")
     ax1.set_ylabel(r"$<x^{n}(0) x^{n}(\tau)>$")
 
-    ax1.errorbar(tau_corr_axis,xCor_av,xCor_err,color = 'blue',fmt = 'o', label = r"$<x(0)x(\tau)>$")
-    ax1.errorbar(tau_corr_axis,xCor_2_av,xCor_2_err,color = 'red',fmt = 'o', label = r"$<x^{2}(0)x^{2}(\tau)>$")
-    ax1.errorbar(tau_corr_axis,xCor_3_av,xCor_3_err,color = 'green',fmt = 'o', label = r"$<x^{3}(0)x^{3}(\tau)>$")
-    ax1.legend(loc = 1)
+    ax1.errorbar(tau_corr_axis, x_cor_av, x_cor_err, color='blue',
+                 fmt='o', label=r"$<x(0)x(\tau)>$")
 
-    Fig1.savefig("Graph_corr_fun.png", dpi = 200)
+    ax1.errorbar(tau_corr_axis, x_cor_2_av, x_cor_2_err, color='red',
+                 fmt='o', label=r"$<x^{2}(0)x^{2}(\tau)>$")
 
-    Fig2 = plt.figure(2)
+    ax1.errorbar(tau_corr_axis, x_cor_3_av, x_cor_3_err, color='green',
+                 fmt='o', label=r"$<x^{3}(0)x^{3}(\tau)>$")
+    ax1.legend(loc=1)
 
-    ax2 = Fig2.add_axes((left,bottom,width,height), facecolor="#e1e1e1")
+    fig1.savefig("Graph_corr_fun.png", dpi=200)
+
+    # Read the output file in order to print the histogram
+
+    with open('hist.dat', 'rb') as reader:
+    # Binary content of the file, we saved data as double
+        hist_raw = reader.read((n_mc_sweeps-n_equil)*n_lattice*8)
+
+    x_config_hist = np.array(struct.unpack(
+            'd'*(n_mc_sweeps-n_equil)*n_lattice, hist_raw))
+
+    #Print histogram for wave function dist.
+    fig2 = plt.figure(2)
+
+    ax2 = fig2.add_axes((0.1, 0.1, 0.8, 0.8), facecolor="#e1e1e1")
 
     ax2.set_xlabel("x")
     ax2.set_ylabel("P(x)")
 
-    ax2.hist(x_Config_hist,x_nHist,(-1.5*f,1.5*f), density = True, color = 'b')
+    ax2.hist(x_config_hist, n_bin_hist,
+            (-1.5*x_potential_minimum, 1.5*x_potential_minimum),
+             density=True,
+             color='b')
 
-    Fig2.savefig("Hist_prob_dens.png", dpi = 200)
+    fig2.savefig("Hist_prob_dens.png", dpi=200)
 
     plt.show()
 
 if __name__ == '__main__':
 
-        Monte_Carlo_AO(1.4,800,0.05,100,100000,0.5,20,5,100,False)
+    monte_carlo_ao(1.4, 800, 0.05, 100, 10000, 0.5, 20, 5, 100, False)
