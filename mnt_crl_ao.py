a4adc81cd5574f91d8679c2c3b9305ef076e3992
Federt1 <75780948+Federt1@users.noreply.github.com>
2022-10-11 16:19:58 +0200

    Some updates

--- mnt_crl_ao.py
+++ mnt_crl_ao.py
@@ -6,21 +6,83 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
-def monte_carlo_ao(x_potential_minimum,
-                   n_lattice,
-                   dtau,
-                   n_equil,
-                   n_mc_sweeps,
-                   delta_x,
-                   n_point,
-                   n_meas,
-                   n_print,
-                   icold):
-    '''Solve the anharmonic oscillator through Monte Carlo technique on an Euclidian Axis'''
+def stat_av_var(observable, observable_sqrd, n_data):
+    '''Evaluate the average and the variance of the average of a set of data,
+    expressed in an array, directly as the sum and the sum of squares.
+    We use the formula Var[<O>] = (<O^2> - <O>^2)/N'''
+    # Control
+    n_array = 0
+
+    if observable.size == observable_sqrd.size:
+        n_array = observable.size
+    else:
+        return None, None
+
+    observable_av = np.empty((n_array), float)
+    observable_err = np.empty((n_array), float)
+    var_temp = 0.0
+
+    for i_array in range(n_array):
+        observable_av[i_array] = observable[i_array]/n_data
+        var_temp = observable_sqrd[i_array]/(n_data*n_data) - \
+            pow(observable_av[i_array], 2)/n_data
+        if var_temp > 0.0:
+            observable_err[i_array] = pow(var_temp, 1/2)
+        else:
+            observable_err[i_array] = 0.
+
+    return observable_av, observable_err
+
+
+def log_central_der_alg(corr_funct, corr_funct_err, delta_step):
+    '''Log derivative of the correlation functions.
+    We can not use the analytic formula because
+    we do not know the energy eignevalues.'''
+    n_array = 0
+
+    if corr_funct.size == corr_funct_err.size:
+        n_array = corr_funct.size
+        if n_array < 2:
+            print("No diff possible")
+            return None, None
+    else:
+        return None, None
+
+    # Reference method
+    derivative_log = np.empty((n_array-1), float)
+    derivative_log_err = np.empty((n_array-1), float)
+
+    for i_array in range(n_array-1):
+        derivative_log[i_array] = - ((corr_funct[i_array+1] -
+                                      corr_funct[i_array]) /
+                                     (corr_funct[i_array]*delta_step))
+
+        derivative_log_err[i_array] = pow(pow(corr_funct_err[i_array+1] /
+                                              corr_funct[i_array], 2) +
+                                          pow(corr_funct_err[i_array] *
+                                              corr_funct[i_array+1] /
+                                              pow(corr_funct[i_array], 2),
+                                              2), 1/2) / delta_step
+
+    return derivative_log, derivative_log_err
+
+
+def monte_carlo_ao(x_potential_minimum,  # potential well position
+                   n_lattice,  # size of the grid
+                   dtau,  # grid spacing in time
+                   n_equil,  # equilibration sweeps
+                   n_mc_sweeps,  # monte carlo sweeps
+                   delta_x,  # width of gauss. dist. for update x
+                   n_point,  #
+                   n_meas,  #
+                   icold):  # cold/hot start):
+    '''Solve the anharmonic oscillator through
+    Monte Carlo technique on an Euclidian Axis'''
+
     if n_mc_sweeps < n_equil:
         print("too few Monte Carlo sweeps/ N_equilib > N_Monte_Carlo")
 
-    # Correlation
+    # Correlation functions
     x_cor_sum = np.zeros((n_point))
     x_cor_2_sum = np.zeros((n_point))
     x_cor_3_sum = np.zeros((n_point))
@@ -30,7 +92,6 @@ def monte_carlo_ao(x_potential_minimum,
     x2_cor_3_sum = np.zeros((n_point))
 
     # Averages and errors
-
     x_cor_av = np.zeros((n_point))
     x_cor_err = np.zeros((n_point))
 
@@ -40,15 +101,18 @@ def monte_carlo_ao(x_potential_minimum,
     x_cor_3_av = np.zeros((n_point))
     x_cor_3_err = np.zeros((n_point))
 
+    derivative_log_corr_funct = np.empty((n_point-1))
+    derivative_log_corr_funct_2 = np.empty((n_point))
+    derivative_log_corr_funct_3 = np.empty((n_point-1))
+
+    derivative_log_corr_funct_err = np.empty((n_point-1))
+    derivative_log_corr_funct_2_err = np.empty((n_point))
+    derivative_log_corr_funct_3_err = np.empty((n_point-1))
+
     # x position along the tau axis
     x_config = np.zeros((n_lattice+1))
 
-    #histogram for the wave function
-
-    n_bin_hist = 100
-
     # initialize the lattice
-
     rnd_uniform = rnd.Random(time.time())
     rnd_gauss = rnd.Random(time.time())
 
@@ -59,7 +123,8 @@ def monte_carlo_ao(x_potential_minimum,
         rnd.seed()
         for i in range(n_lattice):
             x_config[i] = 2 * \
-                rnd_uniform.uniform(0., 1.)*x_potential_minimum - x_potential_minimum
+                rnd_uniform.uniform(0., 1.)*x_potential_minimum \
+                - x_potential_minimum
 
     # impose periodic boundary conditions and
     # understand the way in which they are imposed in the reference file
@@ -89,8 +154,8 @@ def monte_carlo_ao(x_potential_minimum,
         # Equilibration cycle
         while n_config < n_equil:
             for i in range(1, n_lattice):
-                #we apply Metropolis algorithm for each site tau_k:
-                #Secundary cycle
+                # we apply Metropolis algorithm for each site tau_k:
+                # Secundary cycle
                 n_hit += 1
 
                 action_loc_old = (
@@ -105,12 +170,13 @@ def monte_carlo_ao(x_potential_minimum,
                 action_loc_new = (
                     pow((x_new - x_config[i-1])/(2*dtau), 2) +
                     pow((x_config[i+1] - x_new)/(2*dtau), 2) +
-                    pow(x_new * x_new-x_potential_minimum*x_potential_minimum, 2)
-                )*dtau
+                    pow(x_new * x_new -
+                        x_potential_minimum*x_potential_minimum, 2))*dtau
 
                 delta_action = action_loc_new - action_loc_old
 
-                # we put a bound on the value of delta_S because we need the exp.
+                # we put a bound on the value of delta_S
+                # because we need the exp.
                 delta_action = max(delta_action, -70.0)
                 delta_action = min(delta_action, 70.0)
                 # Metropolis question:
@@ -125,7 +191,7 @@ def monte_carlo_ao(x_potential_minimum,
 
         n_config = 0
 
-        #Rest of the MC sweeps
+        # Rest of the MC sweeps
         while (n_config + n_equil) < n_mc_sweeps:
             for i in range(1, n_lattice):
                 n_hit += 1
@@ -142,12 +208,11 @@ def monte_carlo_ao(x_potential_minimum,
                 action_loc_new = (
                     pow((x_new - x_config[i-1])/(2*dtau), 2) +
                     pow((x_config[i+1] - x_new)/(2*dtau), 2) +
-                    pow(x_new * x_new-x_potential_minimum*x_potential_minimum, 2)
-                )*dtau
+                    pow(x_new * x_new -
+                        x_potential_minimum*x_potential_minimum, 2))*dtau
 
                 delta_action = action_loc_new - action_loc_old
 
-                # we put a bound on the value of delta_S because we need the exp.
                 delta_action = max(delta_action, -70.0)
                 delta_action = min(delta_action, 70.0)
                 # Metropolis question:
@@ -166,8 +231,8 @@ def monte_carlo_ao(x_potential_minimum,
             # Calculate the total action:
             for j in range(n_lattice-1):
                 kin_en_temp = pow((x_config[j+1] - x_config[j])/(2*dtau), 2)
-                pot_en_temp = pow(
-                    x_config[j] * x_config[j]-x_potential_minimum*x_potential_minimum, 2)
+                pot_en_temp = pow(x_config[j] * x_config[j] -
+                                  x_potential_minimum*x_potential_minimum, 2)
 
                 kin_en_total += kin_en_temp
                 pot_en_total += pot_en_temp
@@ -176,14 +241,9 @@ def monte_carlo_ao(x_potential_minimum,
                 kin_en_temp = 0.0
                 pot_en_temp = 0.0
 
-            # output control at each n_print step
-
-            #if( (k % n_print) == 0 ):
-                #print('acceptance ratio = {n}'.format(n= n_accept/n_hit))
-
             for k_meas in range(n_meas):
                 n_corr += 1
-                
+
                 i_p0 = int((n_lattice - n_point) * rnd_uniform.uniform(0., 1.))
                 x_0 = x_config[i_p0]
                 for i_corr in range(n_point):
@@ -197,51 +257,39 @@ def monte_carlo_ao(x_potential_minimum,
                     x2_cor_2_sum[i_corr] += pow(x_0 * x_1, 4)
                     x2_cor_3_sum[i_corr] += pow(x_0 * x_1, 6)
                     # Close the output file
-
+            
             n_config += 1
-
     # Monte Carlo End
-    #Evaluate averages and other stuff, maybe we can create a function
-    var = 0.0
-    var2 = 0.0
-    var3 = 0.0
+    # Evaluate averages and other stuff, maybe we can create a function
+    x_cor_av, x_cor_err = stat_av_var(x_cor_sum, x2_cor_sum, n_corr)
+    x_cor_2_av, x_cor_2_err = stat_av_var(x_cor_2_sum, x2_cor_2_sum, n_corr)
+    x_cor_3_av, x_cor_3_err = stat_av_var(x_cor_3_sum, x2_cor_3_sum, n_corr)
 
-    for i_corr in range(n_point):
-        x_cor_av[i_corr] = x_cor_sum[i_corr]/n_corr
-        var = x2_cor_sum[i_corr]/(n_corr*n_corr) - \
-            pow(x_cor_av[i_corr], 2)/n_corr
+    # Correlation function Log
+    derivative_log_corr_funct, derivative_log_corr_funct_err = \
+        log_central_der_alg(x_cor_av, x_cor_err, dtau)
 
-        x_cor_2_av[i_corr] = x_cor_2_sum[i_corr]/n_corr
-        var2 = x2_cor_2_sum[i_corr] / (n_corr*n_corr) - \
-            pow(x_cor_2_av[i_corr], 2)/n_corr
+    # In the case of log <x^2x^2> the constant part <x^2>
+    # is circa the average for the greatest tau
 
-        x_cor_3_av[i_corr] = x_cor_3_sum[i_corr]/n_corr
-        var3 = x2_cor_3_sum[i_corr] / (n_corr*n_corr) - \
-            pow(x_cor_3_av[i_corr], 2)/n_corr
+    derivative_log_corr_funct_2, derivative_log_corr_funct_2_err = \
+        log_central_der_alg(
+            x_cor_2_av - x_cor_2_av[n_point-1],
+            np.sqrt(x_cor_2_err * x_cor_2_err + pow(x_cor_2_err, 2)),
+            dtau)
 
-        if var > 0.0:
-            x_cor_err[i_corr] = pow(var, 1/2)
-        else:
-            x_cor_err[i_corr] = 0.
+    derivative_log_corr_funct_3, derivative_log_corr_funct_3_err = \
+        log_central_der_alg(x_cor_3_av, x_cor_3_err, dtau)
 
-        if var2 > 0.0:
-            x_cor_2_err[i_corr] = pow(var2, 1/2)
-        else:
-            x_cor_2_err[i_corr] = 0.
-
-        if var3 > 0.0:
-            x_cor_3_err[i_corr] = pow(var3, 1/2)
-        else:
-            x_cor_3_err[i_corr] = 0.
-
-    #Control Acceptance ratio
+    # Control Acceptance ratio
     print('acceptance ratio = {n}'.format(n=n_accept/n_hit))
 
     # In order for matplotlib to print text in LaTeX
     plt.rcParams.update({"text.usetex": True, "font.family": "Helvetica"})
 
-    #Print correlation function plot
-    tau_corr_axis = np.arange(0, n_point*dtau, dtau)
+    # Print correlation function plot
+    tau_corr_axis = np.linspace(0, n_point*dtau, n_point, False)
+
     fig1 = plt.figure(1, figsize=(8, 2.5), facecolor="#f1f1f1")
     ax1 = fig1.add_axes((0.1, 0.1, 0.8, 0.8), facecolor="#e1e1e1")
 
@@ -250,42 +298,80 @@ def monte_carlo_ao(x_potential_minimum,
 
     ax1.errorbar(tau_corr_axis, x_cor_av, x_cor_err, color='blue',
                  fmt='o', label=r"$<x(0)x(\tau)>$")
-
     ax1.errorbar(tau_corr_axis, x_cor_2_av, x_cor_2_err, color='red',
                  fmt='o', label=r"$<x^{2}(0)x^{2}(\tau)>$")
-
     ax1.errorbar(tau_corr_axis, x_cor_3_av, x_cor_3_err, color='green',
                  fmt='o', label=r"$<x^{3}(0)x^{3}(\tau)>$")
     ax1.legend(loc=1)
 
     fig1.savefig("Graph_corr_fun.png", dpi=200)
 
-    # Read the output file in order to print the histogram
+    # Print log correlation function plot
+    fig2 = plt.figure(2, figsize=(8, 2.5), facecolor="#f1f1f1")
+    ax2 = fig2.add_axes((0.1, 0.1, 0.8, 0.8), facecolor="#e1e1e1")
 
+    ax2.set_xlabel(r"$\tau$")
+    ax2.set_ylabel(r"$-d \log \big( <x^{n}(0) x^{n}(\tau)> \big)/ \, d\tau$")
+
+    ax2.errorbar(tau_corr_axis[0:(n_point-1)],
+                 derivative_log_corr_funct,
+                 derivative_log_corr_funct_err,
+                 color='blue',
+                 fmt='o',
+                 label=r"$-d \log \big( <x(0) x(\tau)> \big) / \, d\tau$")
+    ax2.errorbar(tau_corr_axis[0:(n_point-1)],
+                 derivative_log_corr_funct_2,
+                 derivative_log_corr_funct_2_err,
+                 color='red',
+                 fmt='o',
+                 label=r"$-d \log \big( <x^2(0) x^2(\tau)> \big) / \, d\tau$")
+    ax2.errorbar(tau_corr_axis[0:(n_point-1)],
+                 derivative_log_corr_funct_3,
+                 derivative_log_corr_funct_3_err,
+                 color='green',
+                 fmt='o',
+                 label=r"$-d \log \big( <x^3(0) x^3(\tau)> \big) / \, d\tau$")
+    ax2.set_ylim((0.0, 5.0))
+    ax2.legend(loc=1)
+
+    fig2.savefig("Graph_log_corr_fun.png", dpi=200)
+
+    # Read the output file in order to print the histogram
     with open('hist.dat', 'rb') as reader:
-    # Binary content of the file, we saved data as double
-        hist_raw = reader.read((n_mc_sweeps-n_equil)*n_lattice*8)
+        # Binary content of the file, we saved data as double
+        hist_raw = reader.read((n_mc_sweeps - n_equil) * n_lattice * 8)
 
     x_config_hist = np.array(struct.unpack(
             'd'*(n_mc_sweeps-n_equil)*n_lattice, hist_raw))
 
-    #Print histogram for wave function dist.
-    fig2 = plt.figure(2)
+    # Print histogram for wave function dist.
+    fig3 = plt.figure(3, figsize=(8, 2.5), facecolor="#f1f1f1")
+    ax3 = fig3.add_axes((0.1, 0.1, 0.8, 0.8), facecolor="#e1e1e1")
 
-    ax2 = fig2.add_axes((0.1, 0.1, 0.8, 0.8), facecolor="#e1e1e1")
+    ax3.set_xlabel("x")
+    ax3.set_ylabel("P(x)")
 
-    ax2.set_xlabel("x")
-    ax2.set_ylabel("P(x)")
+    # number of bins
+    n_bin_hist = 100
 
-    ax2.hist(x_config_hist, n_bin_hist,
-            (-1.5*x_potential_minimum, 1.5*x_potential_minimum),
+    ax3.hist(x_config_hist, n_bin_hist,
+             (-1.5*x_potential_minimum, 1.5*x_potential_minimum),
              density=True,
              color='b')
 
-    fig2.savefig("Hist_prob_dens.png", dpi=200)
+    fig3.savefig("Hist_prob_dens.png", dpi=200)
 
     plt.show()
 
+
 if __name__ == '__main__':
 
-    monte_carlo_ao(1.4, 800, 0.05, 100, 10000, 0.5, 20, 5, 100, False)
+    monte_carlo_ao(1.4,  # x_potential_minimum
+                   800,  # n_lattice
+                   0.05,  # dtau
+                   100,  # n_equil
+                   20000,  # n_mc_sweeps
+                   0.45,  # delta_x
+                   20,  # n_point
+                   5,  # n_meas
+                   False)
---
+++ monte_carlo_ao_switching.py
@@ -0,0 +1,271 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Oct  9 15:12:54 2022
+
+@author: Federico
+"""
+import time
+import random as rnd
+import numpy as np
+import matplotlib.pyplot as plt
+
+def free_energy_harmonic_osc(beta, w_omega0):
+    '''
+    Return the Free Helmoltz energy for an harmonic oscillator
+    '''
+    return -np.log(2.0 * np.sinh(beta * w_omega0 / 2.0)) / beta
+
+
+def stat_av_var(observable, observable_sqrd, n_data):
+    '''
+    Evaluate the average and the variance of the average of a set of data,
+    expressed in an array, directly as the sum and the sum of squares.
+    We use the formula Var[<O>] = (<O^2> - <O>^2)/N
+    '''
+    # Control
+    n_array = 0
+
+    if observable.size == observable_sqrd.size:
+        n_array = observable.size
+    else:
+        return None, None
+
+    observable_av = observable / n_data
+
+    observable_err = np.empty((n_array), float)
+    var_temp = 0.0
+
+    for i_array in range(n_array):
+        var_temp = observable_sqrd[i_array]/(n_data * n_data) - \
+            pow(observable_av[i_array], 2) / n_data
+        if var_temp > 0.0:
+            observable_err[i_array] = var_temp
+        else:
+            observable_err[i_array] = 0.0
+
+    return observable_av, observable_err
+
+
+def monte_carlo_ao_switching(x_potential_minimum,  # potential well position
+                             n_lattice,  # size of the grid
+                             dtau,  # grid spacing in time
+                             n_equil,  # equilibration sweeps
+                             n_mc_sweeps,  # monte carlo sweeps
+                             delta_x,  # width of gauss. dist. for update x
+                             n_switching,  #
+                             w_omega0,  #
+                             icold):  # cold/hot start
+    '''
+    Solve the anharmonic oscillator through
+    Monte Carlo technique on an Euclidian Axis
+    '''
+
+    if n_mc_sweeps < n_equil:
+        print("too few Monte Carlo sweeps/ N_equilib > N_Monte_Carlo")
+
+    # Variables for switching process
+    dalpha = 1.0 / (n_switching)
+    a_alpha = 0.0  # value of the coulping constant
+
+    delta_s_alpha = np.zeros((2*n_switching))
+    delta_s_alpha2 = np.zeros((2*n_switching))
+
+    #delta_s_alpha_av = np.zeros((2*n_switching))
+    #delta_s_alpha_err = np.zeros((2*n_switching))
+
+
+    # x position along the tau axis
+    x_config = np.zeros((n_lattice+1))
+
+
+    if icold is True:
+        for i in range(1,n_lattice):
+            x_config[i] = - x_potential_minimum
+    else:
+        rnd.seed()
+        for i in range(1,n_lattice):
+            x_config[i] = 2 * \
+                rnd.uniform(0., 1.) * x_potential_minimum \
+                - x_potential_minimum
+
+    # impose periodic boundary conditions and
+    # understand the way in which they are imposed in the reference file
+    x_config[0] = x_config[n_lattice-1]
+    x_config[n_lattice] = x_config[1]
+
+    # Now the principal cycle is over the coupling constant alpha
+    for i_switching in range(2 * n_switching):
+
+        if i_switching <= n_switching:
+            a_alpha = i_switching * dalpha
+        else:
+            a_alpha = 2.0 - (i_switching) * dalpha
+
+        # Monte Carlo sweeps
+        
+        # Counters for different parts
+        n_accept = 0
+        n_hit = 0
+        n_config = n_mc_sweeps - n_equil
+
+        # Monte Carlo
+        for i_mc in range(n_mc_sweeps):
+
+            delta_s_alpha_temp = 0.0
+
+            for i in range(1, n_lattice):
+
+                n_hit += 1
+
+                v0 = pow(w_omega0 * x_config[i], 2) / 4.0
+                v1 = pow((pow(x_config[i], 2) - pow(x_potential_minimum, 2)), 2)
+
+                action_loc_old = (
+                    pow((x_config[i] - x_config[i-1])/(2 * dtau), 2) +
+                    pow((x_config[i+1] - x_config[i])/(2 * dtau), 2) + 
+                    (v1 - v0) * a_alpha + v0  # Potential V_alpha
+                    ) * dtau
+
+                x_new = x_config[i] + rnd.gauss(0, delta_x)
+
+                v0 = pow(w_omega0 * x_new, 2) / 4.0
+                v1 = pow((pow(x_new, 2) - pow(x_potential_minimum, 2)), 2)
+
+                action_loc_new = (
+                    pow((x_new - x_config[i-1])/(2*dtau), 2) +
+                    pow((x_config[i+1] - x_new)/(2*dtau), 2) +
+                    (v1 - v0) * a_alpha + v0  # Potential V_alpha
+                    ) * dtau
+
+                delta_action = action_loc_new - action_loc_old
+
+                # we put a bound on the value of delta_S
+                # because we need the exp.
+                delta_action = max(delta_action, -70.0)
+                delta_action = min(delta_action, 70.0)
+                # Metropolis question:
+                if np.exp(-delta_action) > rnd.uniform(0., 1.):
+                    x_config[i] = x_new
+                    n_accept += 1
+
+            x_config[0] = x_config[n_lattice-1]
+            x_config[n_lattice] = x_config[1]
+
+            if i_mc >= n_equil:
+                delta_v_temp = 0.0
+
+                for j in range(n_lattice):
+                    v0 = pow(w_omega0 * x_config[i], 2) / 4.0
+                    v1 = pow((pow(x_config[i], 2) - pow(x_potential_minimum, 2)), 2)
+                    delta_v_temp += (v1 - v0) * dtau
+
+                delta_s_alpha_temp += delta_v_temp
+
+            delta_s_alpha[i_switching] += delta_s_alpha_temp
+            delta_s_alpha2[i_switching] += pow(delta_s_alpha_temp, 2)
+
+
+        # Monte Carlo End
+        # Control Acceptance ratio
+        print('acceptance ratio = {n}'.format(n=n_accept/n_hit))
+
+    
+    delta_s_alpha_av, delta_s_alpha_err = stat_av_var(delta_s_alpha, delta_s_alpha2, n_config)
+    delta_s_alpha_err = np.power(delta_s_alpha_err,2)
+    
+
+    delta_s_alpha_av = np.append(delta_s_alpha_av, delta_s_alpha_av[0])
+    delta_s_alpha_err = np.append(delta_s_alpha_err, delta_s_alpha_err[0])
+
+    print(delta_s_alpha_av)
+
+    # Integration over alpha in <deltaS>
+    integral_01 = np.trapz(delta_s_alpha_av[0:(n_switching + 1)], dx = dalpha)
+    integral_10 = np.trapz(delta_s_alpha_av[n_switching:(2*n_switching + 1)], dx = dalpha)
+    print(integral_01)
+    print(integral_10)
+    integral_01_err = np.trapz(delta_s_alpha_err[0:(n_switching + 1)], dx = dalpha)
+    integral_10_err = np.trapz(delta_s_alpha_err[n_switching:(2*n_switching + 1)], dx = dalpha)
+    trapezoidal_error = np.abs(
+        (delta_s_alpha_av[n_switching] - delta_s_alpha_av[n_switching-1] +
+          delta_s_alpha_av[1] - delta_s_alpha_av[0]) /
+        (dalpha * 12 * n_switching * n_switching ) )
+    propagation_error = np.sqrt(integral_01_err + integral_10_err)
+    hysteresis_error = np.abs(integral_01 - integral_10)
+
+    return -(integral_01 + integral_10) / 2.0, np.sqrt(pow(propagation_error, 2) +
+                                                      pow(hysteresis_error, 2) +
+                                                      pow(trapezoidal_error, 2))
+
+
+def total(n_beta):
+
+    rnd.seed(time.time())   
+
+    # Free Helmoltz energy for the anharmonic oscillator
+    beta_min = 1.0
+    beta_max = 10
+    beta_axis = np.linspace(beta_min, beta_max, n_beta, False)
+    temperature_axis = 1.0 / beta_axis
+
+    dtau = 0.05
+
+    print(beta_axis)
+    print(temperature_axis)
+
+    free_energy = np.empty((n_beta), float)
+    free_energy_err = np.empty((n_beta), float)
+
+    for i_beta in range(n_beta):
+
+        n_lattice = int(beta_axis[i_beta]/dtau)
+
+        print(n_lattice)
+
+        free_energy[i_beta], free_energy_err[i_beta] = \
+        monte_carlo_ao_switching(1.4,
+                                 20,  # n_lattice
+                                 0.05,  # dtau
+                                 100,  # n_equil
+                                 100000,  # n_mc_sweeps
+                                 0.45,
+                                 20,
+                                 1.4 * 4,
+                                 False)
+        free_energy[i_beta] /= beta_axis[i_beta]
+        free_energy_err[i_beta] /= beta_axis[i_beta]
+        free_energy[i_beta] += free_energy_harmonic_osc(beta_axis[i_beta], 1.4*4)
+
+    print(free_energy)
+    print(beta_axis)
+
+    fig1 = plt.figure(1, figsize=(8, 2.5), facecolor="#f1f1f1")
+    ax1 = fig1.add_axes((0.1, 0.1, 0.8, 0.8), facecolor="#e1e1e1")
+
+    ax1.set_xlabel(r"$T$")
+    ax1.set_ylabel(r"$F$")
+
+    ax1.errorbar(temperature_axis,
+                  free_energy,
+                  free_energy_err,
+                  color='b',
+                  fmt='o')
+
+    ax1.set_xscale('log')
+
+    temperature_axis = np.loadtxt('temp.txt', float, delimiter = '\n')
+    free_energy = np.loadtxt('free_energy.txt', float, delimiter = '\n')
+
+    ax1.plot(temperature_axis,
+            free_energy,
+            color='blue')
+
+    fig1.savefig("Free_energy_mc.png", dpi = 200)
+
+    plt.show()
+
+
+if __name__ == '__main__':
+
+    total(1)
+
