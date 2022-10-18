de43da3a7894477085eff964a821718311bd5a4c
Federt1 <75780948+Federt1@users.noreply.github.com>
2022-10-18 17:38:40 +0200

    Updates
    Monte carlo and monte carlo switching are in a more compact form and also virial part

--- mnt_crl_ao.py
+++ mnt_crl_ao.py
@@ -1,111 +1,8 @@
-'''
-
-'''
 import random as rnd
 import numpy as np
 import utility_custom
-<<<<<<< Updated upstream
-
-
-def stat_av_var(observable, observable_sqrd, n_data):
-    '''Evaluate the average and the variance of the average of a set of data,
-    expressed in an array, directly as the sum and the sum of squares.
-    We use the formula Var[<O>] = (<O^2> - <O>^2)/N'''
-    # Control
-    if observable.size != observable_sqrd.size:
-        return None, None
-
-    observable_av = observable / n_data
-    observable_var = observable_sqrd / (n_data * n_data)
-    observable_var -= (np.square(observable_av) / n_data)
-
-    return observable_av, np.sqrt(observable_var)
-
-
-def log_central_der_alg(corr_funct, corr_funct_err, delta_step):
-    '''Log derivative of the correlation functions.
-    We can not use the analytic formula because
-    we do not know the energy eignevalues.'''
-
-    if corr_funct.size != corr_funct_err.size:
-        return None, None
-
-    n_array = corr_funct.size
-    # Reference method
-    derivative_log = np.empty((n_array-1), float)
-    derivative_log_err = np.empty((n_array-1), float)
-
-    for i_array in range(n_array-1):
-        derivative_log[i_array] = - (corr_funct[i_array+1] -corr_funct[i_array]) \
-                                    / (corr_funct[i_array]*delta_step)
-
-        derivative_log_err[i_array] = pow(
-            pow(corr_funct_err[i_array+1] / corr_funct[i_array], 2)
-            + pow(corr_funct_err[i_array] * corr_funct[i_array+1]
-            / pow(corr_funct[i_array], 2), 2), 1/2) / delta_step
-
-    return derivative_log, derivative_log_err
-
-
-def initialize_lattice(n_lattice,
-                       i_cold,
-                       x_potential_minimum):
-    if i_cold is True:
-        x_config = np.repeat(-x_potential_minimum, n_lattice + 1)
-    else:
-        x_config = np.random.uniform(-x_potential_minimum,
-                                     x_potential_minimum,
-                                     n_lattice + 1)
-        x_config[n_lattice-1] = x_config[0]
-        x_config[n_lattice] = x_config[1]
-
-    return x_config
-
-
-def metropolis_question(x_config,
-                        dtau,
-                        delta_x,
-                        x_potential_minimum):
-    '''
-    '''
-    n_lattice = x_config.size - 1
-
-    for i in range(1, n_lattice):
-
-        action_loc_old = (
-            pow((x_config[i] - x_config[i-1])/(2*dtau), 2) +
-            pow((x_config[i+1] - x_config[i])/(2*dtau), 2) +
-            pow(x_config[i] * x_config[i] -
-                x_potential_minimum*x_potential_minimum, 2)
-        )*dtau
-
-        x_new = x_config[i] + rnd.gauss(0, delta_x)
-
-        action_loc_new = (
-            pow((x_new - x_config[i-1])/(2*dtau), 2) +
-            pow((x_config[i+1] - x_new)/(2*dtau), 2) +
-            pow(x_new * x_new -
-                x_potential_minimum*x_potential_minimum, 2))*dtau
-
-        delta_action = action_loc_new - action_loc_old
-
-        # we put a bound on the value of delta_S
-        # because we need the exp.
-        delta_action = max(delta_action, -70.0)
-        delta_action = min(delta_action, 70.0)
-        # Metropolis question:
-        if np.exp(-delta_action) > rnd.uniform(0., 1.):
-            x_config[i] = x_new
-
-    x_config[0] = x_config[n_lattice-1]
-    x_config[n_lattice] = x_config[1]
-
-    return x_config
-
-=======
 import utility_monte_carlo as mc
 import input_parameters as ip
->>>>>>> Stashed changes
 
 def monte_carlo_ao(n_lattice,  # size of the grid
                    n_equil,  # equilibration sweeps
@@ -129,32 +26,19 @@ def monte_carlo_ao(n_lattice,  # size of the grid
     x2_cor_sums = np.zeros((3, n_points))
 
     # x position along the tau axis
-<<<<<<< Updated upstream
-    x_config = initialize_lattice(n_lattice, i_cold, x_potential_minimum)
-=======
     x_config = mc.initialize_lattice(n_lattice, i_cold)
->>>>>>> Stashed changes
 
     # Monte Carlo sweeps: Principal cycle
 
     # Equilibration cycle
     for i_equil in range(n_equil):
-<<<<<<< Updated upstream
-        x_config = metropolis_question(x_config, dtau, delta_x, x_potential_minimum)
-=======
         x_config = mc.metropolis_question(x_config)
->>>>>>> Stashed changes
 
         # Rest of the MC sweeps
     with open(output_path + '/ground_state_histogram.dat', 'wb') as hist_writer:
         for i_mc in range(n_mc_sweeps - n_equil):
-<<<<<<< Updated upstream
-            x_config = metropolis_question(x_config, dtau, delta_x, x_potential_minimum)
-            np.save(hist_writer, x_config[0:(n_lattice-1)])
-=======
             x_config = mc.metropolis_question(x_config)
             np.save(hist_writer, x_config[0:(n_lattice - 1)])
->>>>>>> Stashed changes
             for k_meas in range(n_meas):
                 i_p0 = int((n_lattice - n_points) * rnd.uniform(0., 1.))
                 x_0 = x_config[i_p0]
@@ -170,23 +54,17 @@ def monte_carlo_ao(n_lattice,  # size of the grid
                     x2_cor_sums[2, i_point] += pow(x_0 * x_1, 6)
 
     # Evaluate averages and other stuff, maybe we can create a function
-    x_cor_av, x_cor_err = stat_av_var(x_cor_sums[0],
+    x_cor_av, x_cor_err = mc.stat_av_var(x_cor_sums[0],
                                       x2_cor_sums[0],
-                                      n_meas*(n_mc_sweeps - n_equil))
-    x_cor_2_av, x_cor_2_err = stat_av_var(x_cor_sums[1],
+                                      n_meas * (n_mc_sweeps - n_equil))
+    x_cor_2_av, x_cor_2_err = mc.stat_av_var(x_cor_sums[1],
                                           x2_cor_sums[1],
-                                          n_meas*(n_mc_sweeps - n_equil))
-    x_cor_3_av, x_cor_3_err = stat_av_var(x_cor_sums[2],
+                                          n_meas * (n_mc_sweeps - n_equil))
+    x_cor_3_av, x_cor_3_err = mc.stat_av_var(x_cor_sums[2],
                                           x2_cor_sums[2],
-<<<<<<< Updated upstream
-                                          n_meas*(n_mc_sweeps - n_equil))
-    
-    with open(output_path + r'\tau_array.txt', 'w') as tau_writer:
-=======
                                           n_meas * (n_mc_sweeps - n_equil))
 
     with open(output_path + '/tau_array.txt', 'w') as tau_writer:
->>>>>>> Stashed changes
         np.savetxt(tau_writer,
                    np.linspace(0, n_points * ip.dtau, n_points, False))
     with open(output_path + '/average_x_cor.txt', 'w') as av_writer:
@@ -204,26 +82,18 @@ def monte_carlo_ao(n_lattice,  # size of the grid
 
     # Correlation function Log
     derivative_log_corr_funct, derivative_log_corr_funct_err = \
-<<<<<<< Updated upstream
-        log_central_der_alg(x_cor_av, x_cor_err, dtau)
-=======
         mc.log_central_der_alg(x_cor_av, x_cor_err, ip.dtau)
->>>>>>> Stashed changes
 
     # In the case of log <x^2x^2> the constant part <x^2>
     # is circa the average for the greatest tau
 
     derivative_log_corr_funct_2, derivative_log_corr_funct_2_err = \
-        log_central_der_alg(x_cor_2_av - x_cor_2_av[n_points-1],
+        mc.log_central_der_alg(x_cor_2_av - x_cor_2_av[n_points - 1],
                             np.sqrt(x_cor_2_err * x_cor_2_err + pow(x_cor_2_err, 2)),
                             ip.dtau)
 
     derivative_log_corr_funct_3, derivative_log_corr_funct_3_err = \
-<<<<<<< Updated upstream
-        log_central_der_alg(x_cor_3_av, x_cor_3_err, dtau)
-=======
         mc.log_central_der_alg(x_cor_3_av, x_cor_3_err, ip.dtau)
->>>>>>> Stashed changes
 
     with open(output_path + '/average_der_log.txt', 'w') as av_writer:
         np.savetxt(av_writer, derivative_log_corr_funct)
@@ -239,18 +109,3 @@ def monte_carlo_ao(n_lattice,  # size of the grid
         np.savetxt(err_writer, derivative_log_corr_funct_3_err)
 
     return 1
-<<<<<<< Updated upstream
-
-if __name__ == '__main__':
-
-    monte_carlo_ao(1.4,  # x_potential_minimum
-                   800,  # n_lattice
-                   0.05,  # dtau
-                   100,  # n_equil
-                   100000,  # n_mc_sweeps
-                   0.45,  # delta_x
-                   20,  # n_point
-                   5,  # n_meas
-                   False)
-=======
->>>>>>> Stashed changes
--- monte_carlo_ao_switching.py
+++ monte_carlo_ao_switching.py
@@ -5,20 +5,16 @@ import time
 import random as rnd
 import numpy as np
 
-<<<<<<< Updated upstream
-import monte_carlo_ao
-=======
 import utility_monte_carlo as mc
->>>>>>> Stashed changes
 import utility_custom
 import input_parameters as ip
+
 
 def free_energy_harmonic_osc(beta):
     '''
     Return the Free Helmoltz energy for an harmonic oscillator
     '''
     return -np.log(2.0 * np.sinh(beta * ip.w_omega0 / 2.0)) / beta
-
 
 
 def monte_carlo_ao_switching(n_lattice,  # size of the grid
@@ -45,56 +41,31 @@ def monte_carlo_ao_switching(n_lattice,  # size of the grid
     # Now the principal cycle is over the coupling constant alpha
     for i_switching in range(2 * n_switching + 1):
 
-<<<<<<< Updated upstream
-        x_config = monte_carlo_ao.initialize_lattice(n_lattice,
-                                                     i_cold)
-=======
         x_config = mc.initialize_lattice(n_lattice,i_cold)
->>>>>>> Stashed changes
 
         if i_switching <= n_switching:
             a_alpha = i_switching * d_alpha
         else:
             a_alpha = 2.0 - (i_switching) * d_alpha
 
-
         for i_equil in range(n_equil):
-<<<<<<< Updated upstream
-            x_config = monte_carlo_ao.metropolis_question(x_config,
-                                                          a_alpha)
-        for i_mc in range(n_mc_sweeps - n_equil):
-
-            delta_s_alpha_temp = 0.0
-            x_config = monte_carlo_ao.metropolis_question(x_config,
-                                                      a_alpha)
-=======
             x_config = mc.metropolis_question(x_config, a_alpha)
         for i_mc in range(n_mc_sweeps - n_equil):
 
             delta_s_alpha_temp = 0.0
             x_config = mc.metropolis_question(x_config, a_alpha)
->>>>>>> Stashed changes
             for j in range(n_lattice):
                 potential_0 = pow(ip.w_omega0 * x_config[j], 2) / 4.0
                 potential_1 = pow((pow(x_config[j], 2) - pow(ip.x_potential_minimum, 2)), 2)
                 delta_s_alpha_temp += (potential_1 - potential_0) * ip.dtau
 
-
             delta_s_alpha[i_switching] += delta_s_alpha_temp
             delta_s_alpha2[i_switching] += pow(delta_s_alpha_temp, 2)
 
-        # Monte Carlo End
-        # Control Acceptance ratio
-
+    # Evaluate the averages and errors        
     delta_s_alpha_av, delta_s_alpha_err = \
-<<<<<<< Updated upstream
-        monte_carlo_ao.stat_av_var(delta_s_alpha,
-                               delta_s_alpha2,
-                               n_mc_sweeps - n_equil)
-=======
         mc.stat_av_var(delta_s_alpha,delta_s_alpha2,
                        n_mc_sweeps - n_equil)
->>>>>>> Stashed changes
 
     # Integration over alpha in <deltaS>
     integral_01 = np.trapz(delta_s_alpha_av[0:(n_switching + 1)], dx = d_alpha)
@@ -115,79 +86,89 @@ def monte_carlo_ao_switching(n_lattice,  # size of the grid
                                                       pow(hysteresis_error, 2) +
                                                       pow(trapezoidal_error, 2))
 
+def monte_carlo_virial_theorem(n_lattice,  # size of the grid
+                               n_equil,  # equilibration sweeps
+                               n_mc_sweeps,  # monte carlo sweeps
+                               i_cold):
+    
+    # x position along the tau axis
+    x_config = mc.initialize_lattice(n_lattice, i_cold)
+
+    virial_hamiltonian = 0.0
+    virial_hamiltonian2 = 0.0
+    
+    for i_equil in range(n_equil):
+        x_config = mc.metropolis_question(x_config)
+
+    # Rest of the MC sweeps
+    
+    for i_mc in range(n_mc_sweeps - n_equil):
+        x_config = mc.metropolis_question(x_config)
+        pot = 0.0
+        kin = 0.0
+        for i_pos in range(1, n_lattice):
+            pot = pow( pow(x_config[i_pos], 2) 
+                      - pow(ip.x_potential_minimum, 2)
+                      , 2)
+            kin = 4.0 * pow(x_config[i_pos], 2) \
+                 * (pow(x_config[i_pos], 2) 
+                    - pow(ip.x_potential_minimum, 2)
+                   ) / 2.0
+            virial_hamiltonian += (pot + kin)
+            virial_hamiltonian2 += pow(pot + kin, 2)
+            pot = 0.0
+            kin = 0.0
+                
+    return virial_hamiltonian, virial_hamiltonian2
+
+
 def free_energy_anharm(n_beta,
                        n_equil,
                        n_mc_sweeps,
-                       n_switching):
+                       n_switching,
+                       i_cold):
 
     # Control output filepath
-<<<<<<< Updated upstream
-    output_path = r'.\output_data\output_monte_carlo_switching'
-=======
     output_path = './output_data/output_monte_carlo_switching'
->>>>>>> Stashed changes
     utility_custom.output_control(output_path)
 
     rnd.seed(time.time())
 
-    # Free Helmoltz energy for the anharmonic oscillator
-<<<<<<< Updated upstream
-    beta_array = np.linspace(1.0, 8.0, n_beta, False)
-    temperature_array = 1.0 / beta_array
-
-    dtau = 0.05
-=======
+    #Free Helmoltz energy for the anharmonic oscillator
     beta_array = np.linspace(1.0, 2.0, n_beta, False)
     temperature_array = 1.0 / beta_array
->>>>>>> Stashed changes
 
     free_energy = np.empty((n_beta), float)
     free_energy_err = np.empty((n_beta), float)
 
     for i_beta in range(n_beta):
 
-<<<<<<< Updated upstream
-        n_lattice = int(beta_array[i_beta]/dtau)
-=======
         n_lattice = int(beta_array[i_beta]/ip.dtau)
->>>>>>> Stashed changes
 
         free_energy[i_beta], free_energy_err[i_beta] = \
         monte_carlo_ao_switching(n_lattice,  # n_lattice
-                                 n_equil,  # n_equil
-                                 n_mc_sweeps,  # n_mc_sweeps-
-                                 n_switching,
-                                 False)
+                                  n_equil,  # n_equil
+                                  n_mc_sweeps,  # n_mc_sweeps-
+                                  n_switching,
+                                  i_cold)
         free_energy[i_beta] /= beta_array[i_beta]
         free_energy_err[i_beta] /= beta_array[i_beta]
         free_energy[i_beta] += free_energy_harmonic_osc(beta_array[i_beta])
 
-<<<<<<< Updated upstream
-    with open(output_path + r'\free_energy_mc.txt', 'w') as f_writer:
-        np.savetxt(f_writer, free_energy)
-    with open(output_path + r'\free_energy_mc_err.txt', 'w') as f_writer:
-        np.savetxt(f_writer, free_energy_err)
-    with open(output_path + r'\temperature.txt', 'w') as f_writer:
-=======
+    v_ham, v_ham2 = monte_carlo_virial_theorem(400, 
+                                               n_equil, 
+                                               n_mc_sweeps,
+                                               i_cold)
+
+    virial_hamiltonian_av, virial_hamiltonian_err = \
+        mc.stat_av_var(v_ham, v_ham2, 400 * (n_mc_sweeps - n_equil))
+
     with open(output_path + '/free_energy_mc.txt', 'w') as f_writer:
         np.savetxt(f_writer, free_energy)
+        f_writer.write(str(-virial_hamiltonian_av))
     with open(output_path + '/free_energy_mc_err.txt', 'w') as f_writer:
         np.savetxt(f_writer, free_energy_err)
+        f_writer.write(str(virial_hamiltonian_err))
     with open(output_path + '/temperature.txt', 'w') as f_writer:
->>>>>>> Stashed changes
         np.savetxt(f_writer, temperature_array)
-
-    print(free_energy)
-
-if __name__ == '__main__':
-
-<<<<<<< Updated upstream
-    free_energy_anharm(4,
-                       100,
-                       50000,
-=======
-    free_energy_anharm(2,
-                       100,
-                       500,
->>>>>>> Stashed changes
-                       20)
+        f_writer.write(str(1/(400*0.05)))
