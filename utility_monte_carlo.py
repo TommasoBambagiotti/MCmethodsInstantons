24aaa7812fec9f1517819ac0c7d42b6ba343f8ea
TommasoBambagiotti <61661628+TommasoBambagiotti@users.noreply.github.com>
2022-10-18 19:38:46 +0200

    inserito codice cooling e funzioni per il cooling
    print_last_config serve solo a stampare i valori per l'ultima configurazione senza dover fare il cooling di tutte le altre configurazioni (per velocizzare un po' la cosa)

--- utility_monte_carlo.py
+++ utility_monte_carlo.py
@@ -3,21 +3,20 @@ import random as rnd
 import input_parameters as ip
 
 
-
 def potential_alpha(x_position,
                     a_alpha):
-    if(a_alpha > -0.01):
-        potential_1 = pow(x_position * x_position - 
-                   ip.x_potential_minimum * ip.x_potential_minimum, 2)
+    if (a_alpha > -0.01):
+        potential_1 = pow(x_position * x_position -
+                          ip.x_potential_minimum * ip.x_potential_minimum, 2)
         potential_0 = pow(ip.w_omega0 * x_position, 2) / 4.0
         return a_alpha * (potential_1 - potential_0) + potential_0
     else:
-        return pow(x_position * x_position - 
+        return pow(x_position * x_position -
                    ip.x_potential_minimum * ip.x_potential_minimum, 2)
 
 
 def metropolis_question(x_config,
-                        a_alpha = -1.0):
+                        a_alpha=-1.0):
     '''
     '''
     n_lattice = x_config.size - 1
@@ -52,9 +51,9 @@ def metropolis_question(x_config,
 
     return x_config
 
+
 def initialize_lattice(n_lattice,
                        i_cold):
-    
     if i_cold is True:
         x_config = np.repeat(-ip.x_potential_minimum, n_lattice + 1)
     else:
@@ -65,6 +64,7 @@ def initialize_lattice(n_lattice,
         x_config[n_lattice] = x_config[1]
 
     return x_config
+
 
 def stat_av_var(observable, observable_sqrd, n_data):
     '''Evaluate the average and the variance of the average of a set of data,
@@ -79,6 +79,7 @@ def stat_av_var(observable, observable_sqrd, n_data):
     observable_var -= (np.square(observable_av) / n_data)
 
     return observable_av, np.sqrt(observable_var)
+
 
 def log_central_der_alg(corr_funct, corr_funct_err, delta_step):
     '''Log derivative of the correlation functions.
@@ -104,33 +105,81 @@ def log_central_der_alg(corr_funct, corr_funct_err, delta_step):
 
     return derivative_log, derivative_log_err
 
+def configuration_cooling(x_cold_config, dtau, delta_x, x_potential_minimum):
+
+    n_lattice = x_cold_config.size -1
+    n_trials = 10
+
+    for i in range (1,n_lattice):
+
+        action_loc_old = (
+                                 pow((x_cold_config[i] - x_cold_config[i - 1]) / (2 * dtau), 2) +
+                                 pow((x_cold_config[i + 1] - x_cold_config[i]) / (2 * dtau), 2) +
+                                 pow(x_cold_config[i] * x_cold_config[i] -
+                                     x_potential_minimum * x_potential_minimum, 2)
+                         ) * dtau
+        for j in range(1,n_trials): #perchè??
+
+            x_new = x_cold_config[i] + rnd.gauss(0, delta_x)
+
+            action_loc_new = (
+                                         pow((x_new - x_cold_config[i - 1]) / (2 * dtau), 2) +
+                                         pow((x_cold_config[i + 1] - x_new) / (2 * dtau), 2) +
+                                         pow(x_new * x_new -
+                                             x_potential_minimum * x_potential_minimum, 2)) * dtau
+
+            if ((action_loc_new - action_loc_old) < 0 ):
+                x_cold_config[i] = x_new
+
+
+
+    return x_cold_config
+
+
 def find_instantons(x,n_lattice,dt):
 
     pos_roots = 0
     neg_roots = 0
     pos_roots_position = np.zeros(1)
     neg_roots_position = np.zeros(1)
-    i_zero = 0
 
-    while(i_zero < n_lattice) and (x[i_zero] == 0):
-        i_zero +=1
-
-    x_pos = x[i_zero]
     #print(f'x_pos ={x[i_zero]} with i_zero = {i_zero}')
+
+    if x[0] == 0:
+
+        if x[1] - x[0] > 0:
+            pos_roots += 1
+            pos_roots_position = np.append(pos_roots_position, 0)
+            i_zero = 1
+            x_pos = x[i_zero]
+
+
+        elif x[1] - x[0] < 0:
+
+            neg_roots += 1
+            neg_roots_position = np.append(neg_roots_position, 0)
+            i_zero = 1
+            x_pos = x[i_zero]
+    else:
+        i_zero = 0
+        x_pos = x[i_zero]
 
     for i in range(i_zero+1,n_lattice):
 
+
         if x_pos*x[i] < 0 :
 
-            if x[i]-x_pos > 0:
+            if x[i]-x[i-1] > 0:
                 pos_roots += 1
-                pos_roots_position = np.append(pos_roots_position, i*dt)
+                pos_roots_position = np.append(pos_roots_position, (i*dt+(i-1)*dt)/2)
                 x_pos = x[i]
 
-            elif x[i]-x_pos < 0:
+            elif x[i]-x[i-1] < 0:
 
                 neg_roots += 1
-                neg_roots_position = np.append(neg_roots_position,i*dt)
+                neg_roots_position = np.append(neg_roots_position,(i*dt+(i-1)*dt)/2)
                 x_pos = x[i]
+    a = np.delete(pos_roots_position, 0)
+    b = np.delete(neg_roots_position, 0)
 
-    return pos_roots,neg_roots,pos_roots_position,neg_roots_position
+    return pos_roots,neg_roots,a ,b
