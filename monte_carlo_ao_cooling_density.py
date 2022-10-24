bf268aec1d2c98b27d57d1b8634f1c6e3895ef7a
Federt1 <75780948+Federt1@users.noreply.github.com>
2022-10-24 14:01:23 +0200

    Add density
    Nel file utility ho aggiunto la funzione action

---
+++ monte_carlo_ao_cooling_density.py
@@ -0,0 +1,133 @@
+
+import numpy as np
+
+import utility_monte_carlo as mc
+import utility_custom
+import input_parameters as ip
+
+
+def cooled_monte_carlo(
+        n_lattice,
+        n_equil,
+        n_mc_sweeps,
+        i_cold,
+        n_sweeps_btw_cooling,
+        n_cooling_sweeps,
+        n_minima):
+
+
+    if n_mc_sweeps < n_equil:
+        print("too few Monte Carlo sweeps/ N_equilib > N_Monte_Carlo")
+        #return 0
+
+    # Control output filepath
+    output_path = './output_data/output_cooled_monte_carlo'
+    utility_custom.output_control(output_path)
+
+    potential_minima = np.zeros((n_minima), float)
+
+    for i_minimum in range(n_minima):
+        print(f'New monte carlo for eta = {1.4 + 0.1 * i_minimum}')
+
+        #contatore probabilmente inutile
+        n_cooling = 0
+
+        ip.x_potential_minimum = 1.4 + 0.1 * i_minimum
+
+        ip.print_minimum()
+
+        potential_minima[i_minimum] = ip.x_potential_minimum
+
+        # x position along the tau axis
+        x_config = mc.initialize_lattice(n_lattice, i_cold)
+
+        #number of instantons
+
+        n_total_instantons_sum = np.zeros((n_cooling_sweeps), int)
+        n2_total_instantons_sum = np.zeros((n_cooling_sweeps), int)
+
+        action_cooling = np.zeros((n_cooling_sweeps), float)
+
+        # Monte Carlo sweeps: Principal cycle
+
+        # Equilibration cycle
+
+        for i_equil in range(n_equil):
+            mc.metropolis_question(x_config)
+
+        # Rest of the MC sweeps
+
+        for i_mc in range(n_mc_sweeps - n_equil):
+            mc.metropolis_question(x_config)
+
+            # COOLING
+
+            if (i_mc % n_sweeps_btw_cooling) == 0:
+
+                print(f'cooling #{n_cooling} of {(n_mc_sweeps - n_equil) / n_sweeps_btw_cooling}\n'
+                          f'in configuration #{i_mc}')
+
+                x_cold_config = np.copy(x_config)
+                n_cooling += 1
+
+                for i_cooling in range(n_cooling_sweeps):
+                    mc.configuration_cooling(x_cold_config,
+                                             ip.x_potential_minimum)
+
+                    # Find instantons and antiinstantons
+                    n_instantons, n_anti_instantons = mc.find_instantons(x_cold_config,
+                                                                             n_lattice,
+                                                                             ip.dtau)
+                    #pos_instantons, pos_anti_instantons 
+
+                    n_total_instantons_sum[i_cooling] += (n_instantons + n_anti_instantons)
+                    n2_total_instantons_sum[i_cooling] += pow((n_instantons + n_anti_instantons), 2)
+
+                    # action for each cooling configuration
+                    action_cooling[i_cooling] += mc.return_action(x_cold_config)
+
+        # Evaluate averages and errors
+
+        action_cooling /= n_cooling
+
+        n_total, n_total_err = mc.stat_av_var(n_total_instantons_sum,
+                                              n2_total_instantons_sum,
+                                              n_cooling)
+
+        np.savetxt(output_path + '/tau_array_conf.txt', np.linspace(0.0, n_lattice * ip.dtau, n_lattice))
+        np.savetxt(output_path + '/configuration.txt', x_config[0:-1])
+        np.savetxt(output_path + '/configuration_cooled.txt', x_cold_config[0:-1])
+
+        np.savetxt(output_path + f'/n_total_{i_minimum + 1}.txt', n_total)
+
+        with open (output_path + f'/action_{i_minimum + 1}_original.txt', 'w',
+                   encoding='utf-8') as act_writer:
+            np.savetxt(act_writer, action_cooling)
+
+        action_cooling = np.divide(action_cooling, n_total)
+
+
+        n_total /= (n_lattice * ip.dtau * 2.0)
+        n_total_err /= (n_lattice * ip.dtau * 2.0)
+
+        
+
+        with open (output_path + f'/n_instantons_{i_minimum + 1}.txt', 'w',
+                  encoding='utf-8') as n_inst_writer:
+            np.savetxt(n_inst_writer, n_total)
+
+        with open (output_path + f'/n_instantons_{i_minimum + 1}_err.txt', 'w',
+                  encoding='utf-8') as n_inst_writer:
+            np.savetxt(n_inst_writer, n_total_err)
+
+        with open (output_path + f'/action_{i_minimum + 1}.txt', 'w',
+                   encoding='utf-8') as act_writer:
+            np.savetxt(act_writer, action_cooling)
+
+    with open (output_path + '/n_cooling.txt', 'w',
+               encoding='utf-8') as n_inst_writer:
+        np.savetxt(n_inst_writer, np.linspace(1, n_cooling_sweeps + 1, n_cooling_sweeps, False))
+
+    # with open (output_path + '/potential_minima.txt', 'w',
+    #         encoding='utf-8') as n_inst_writer:
+    #     np.savetxt(n_inst_writer, potential_minima)
--- utility_monte_carlo.py
+++ utility_monte_carlo.py
@@ -16,23 +16,24 @@ def potential_alpha(x_position,
 
 def metropolis_question(x_config,
                         a_alpha=-1.0):
+
     n_lattice = x_config.size - 1
 
     for i in range(1, n_lattice):
 
         action_loc_old = (
-                                 pow((x_config[i] - x_config[i - 1]) / (2 * ip.dtau), 2) +
-                                 pow((x_config[i + 1] - x_config[i]) / (2 * ip.dtau), 2) +
-                                 potential_alpha(x_config[i], a_alpha)
+                                 pow((x_config[i] - x_config[i - 1]) / (2 * ip.dtau), 2)
+                                 + pow((x_config[i + 1] - x_config[i]) / (2 * ip.dtau), 2)
+                                 + potential_alpha(x_config[i], a_alpha)
                          ) * ip.dtau
 
         x_new = x_config[i] + rnd.gauss(0, ip.delta_x)
 
         action_loc_new = (
-                                 pow((x_new - x_config[i - 1]) / (2 * ip.dtau), 2) +
-                                 pow((x_config[i + 1] - x_new) / (2 * ip.dtau), 2) +
-                                 potential_alpha(x_new, a_alpha)) * ip.dtau
-
+                                 pow((x_new - x_config[i - 1]) / (2 * ip.dtau), 2)
+                                 + pow((x_config[i + 1] - x_new) / (2 * ip.dtau), 2)
+                                 + potential_alpha(x_new, a_alpha)
+                        ) * ip.dtau
         delta_action = action_loc_new - action_loc_old
 
         # we put a bound on the value of delta_S
@@ -46,7 +47,19 @@ def metropolis_question(x_config,
     x_config[0] = x_config[n_lattice - 1]
     x_config[n_lattice] = x_config[1]
 
-    return x_config
+
+def return_action(x_config):
+    
+    n_lattice = x_config.size - 1
+    action = 0.0
+    
+    for i_pos in range(1, n_lattice):
+        action += (pow((x_config[i_pos] - x_config[i_pos - 1]) / (2 * ip.dtau), 2) 
+            + pow(x_config[i_pos] * x_config[i_pos] -
+                ip.x_potential_minimum * ip.x_potential_minimum, 2)
+            ) * ip.dtau
+        
+    return action
 
 
 def initialize_lattice(n_lattice,
@@ -67,7 +80,6 @@ def stat_av_var(observable, observable_sqrd, n_data):
     '''Evaluate the average and the variance of the average of a set of data,
     expressed in an array, directly as the sum and the sum of squares.
     We use the formula Var[<O>] = (<O^2> - <O>^2)/N'''
-    # Control
 
     if (type(observable) is np.ndarray) and (type(observable_sqrd) is np.ndarray):
         if observable.size != observable_sqrd.size:
@@ -105,32 +117,39 @@ def log_central_der_alg(corr_funct, corr_funct_err, delta_step):
 
     return derivative_log, derivative_log_err
 
-def configuration_cooling(x_cold_config, dtau, delta_x, x_potential_minimum):
+
+def configuration_cooling(x_cold_config,
+                          x_potential_minimum):
     n_lattice = x_cold_config.size - 1
     n_trials = 10
 
     for i in range(1, n_lattice):
 
         action_loc_old = (
-                                 pow((x_cold_config[i] - x_cold_config[i - 1]) / (2 * dtau), 2) +
-                                 pow((x_cold_config[i + 1] - x_cold_config[i]) / (2 * dtau), 2) +
+                                 pow((x_cold_config[i] - x_cold_config[i - 1]) / (2 * ip.dtau), 2) +
+                                 pow((x_cold_config[i + 1] - x_cold_config[i]) / (2 * ip.dtau), 2) +
                                  pow(x_cold_config[i] * x_cold_config[i] -
                                      x_potential_minimum * x_potential_minimum, 2)
-                         ) * dtau
+                         ) * ip.dtau
         for j in range(1, n_trials):  # perchè??
-
-            x_new = x_cold_config[i] + rnd.gauss(0, delta_x)
-
+    
+            x_new = x_cold_config[i] + rnd.gauss(0, ip.delta_x * 0.1)
+    
             action_loc_new = (
-                                     pow((x_new - x_cold_config[i - 1]) / (2 * dtau), 2) +
-                                     pow((x_cold_config[i + 1] - x_new) / (2 * dtau), 2) +
-                                     pow(x_new * x_new -
-                                         x_potential_minimum * x_potential_minimum, 2)) * dtau
-
+                                    pow((x_new - x_cold_config[i - 1]) / (2 * ip.dtau), 2) +
+                                    pow((x_cold_config[i + 1] - x_new) / (2 * ip.dtau), 2) +
+                                    pow(x_new * x_new -
+                                        x_potential_minimum * x_potential_minimum, 2)
+                             ) * ip.dtau
+    
             if ((action_loc_new - action_loc_old) < 0):
-                x_cold_config[i] = x_new
+                    x_cold_config[i] = x_new
 
-    return x_cold_config
+
+    x_cold_config[0] = x_cold_config[n_lattice - 1]
+    x_cold_config[n_lattice] = x_cold_config[1]
+
+
 
 
 def find_instantons(x,n_lattice,dt):
@@ -169,7 +188,7 @@ def find_instantons(x,n_lattice,dt):
 
             if x[i]-x[i-1] > 0:
                 pos_roots += 1
-                pos_roots_position = np.append(pos_roots_position, (i*dt+(i-1)*dt)/2)
+                #pos_roots_position = np.append(pos_roots_position, (i*dt+(i-1)*dt)/2)
                 x_pos = x[i]
 
             elif x[i]-x[i-1] < 0:
@@ -177,8 +196,7 @@ def find_instantons(x,n_lattice,dt):
                 neg_roots += 1
                 neg_roots_position = np.append(neg_roots_position,(i*dt+(i-1)*dt)/2)
                 x_pos = x[i]
-    a = np.delete(pos_roots_position, 0)
-    b = np.delete(neg_roots_position, 0)
+    #a = np.delete(pos_roots_position, 0)
+    #b = np.delete(neg_roots_position, 0)
 
-    return pos_roots,neg_roots,a ,b
-
+    return pos_roots,neg_roots, a ,b
