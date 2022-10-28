17e8f0e7977fa9d5cf48f511229f18a40317ae98
TommasoBambagiotti <61661628+TommasoBambagiotti@users.noreply.github.com>
2022-10-28 11:35:55 +0200

    small change

--- monte_carlo_ao_switching.py
+++ monte_carlo_ao_switching.py
@@ -36,7 +36,7 @@ def monte_carlo_ao_switching(n_lattice,  # size of the grid
 
     delta_s_alpha = np.zeros((2 * n_switching + 1))
     delta_s_alpha2 = np.zeros((2 * n_switching + 1))
-    print(f'Adiabatic switching for beta = {n_lattice * 0.05}')
+    print(f'Adiabatic switching for beta = {n_lattice * ip.dtau}')
     # Now the principal cycle is over the coupling constant alpha
     for i_switching in range(2 * n_switching + 1):
 
@@ -48,10 +48,10 @@ def monte_carlo_ao_switching(n_lattice,  # size of the grid
             a_alpha = 2.0 - (i_switching) * d_alpha
 
         print(f'Switching #{i_switching}')
-        
+
         for i_equil in range(n_equil):
-           mc.metropolis_question(x_config, a_alpha)
-            
+            mc.metropolis_question(x_config, a_alpha)
+
         for i_mc in range(n_equil, n_mc_sweeps):
 
             delta_s_alpha_temp = 0.0
@@ -60,7 +60,7 @@ def monte_carlo_ao_switching(n_lattice,  # size of the grid
                 potential_0 = pow(ip.w_omega0 * x_config[j], 2) / 4.0
                 potential_1 = pow(x_config[j] * x_config[j]
                                   - (ip.x_potential_minimum
-                                  * ip.x_potential_minimum)
+                                     * ip.x_potential_minimum)
                                   , 2)
                 delta_s_alpha_temp += (potential_1 - potential_0) * ip.dtau
 
@@ -84,7 +84,7 @@ def monte_carlo_ao_switching(n_lattice,  # size of the grid
         delta_s_alpha_err[n_switching:(2 * n_switching + 1)], dx=d_alpha)
 
     trapezoidal_error = np.abs(
-        (delta_s_alpha_av[n_switching] - delta_s_alpha_av[n_switching-1] 
+        (delta_s_alpha_av[n_switching] - delta_s_alpha_av[n_switching - 1]
          + delta_s_alpha_av[1] - delta_s_alpha_av[0]) /
         (d_alpha * 12 * n_switching * n_switching))
 
@@ -92,14 +92,14 @@ def monte_carlo_ao_switching(n_lattice,  # size of the grid
     hysteresis_error = np.abs(integral_01 - integral_10)
 
     return -(integral_01 + integral_10) / 2.0, np.sqrt(pow(propagation_error, 2) +
-                                                      pow(hysteresis_error, 2) +
-                                                      pow(trapezoidal_error, 2))
+                                                       pow(hysteresis_error, 2) +
+                                                       pow(trapezoidal_error, 2))
+
 
 def monte_carlo_virial_theorem(n_lattice,  # size of the grid
                                n_equil,  # equilibration sweeps
                                n_mc_sweeps,  # monte carlo sweeps
                                i_cold):
-    
     print('Monte Carlo virial theorem approach:')
     # x position along the tau axis
     x_config = mc.initialize_lattice(n_lattice, i_cold)
@@ -117,18 +117,17 @@ def monte_carlo_virial_theorem(n_lattice,  # size of the grid
         pot = 0.0
         kin = 0.0
         for i_pos in range(1, n_lattice):
-            pot = pow( pow(x_config[i_pos], 2)
+            pot = pow(pow(x_config[i_pos], 2)
                       - pow(ip.x_potential_minimum, 2)
                       , 2)
             kin = 4.0 * pow(x_config[i_pos], 2) \
-                 * (pow(x_config[i_pos], 2)
-                    - pow(ip.x_potential_minimum, 2)
-                   ) / 2.0
+                  * (pow(x_config[i_pos], 2)
+                     - pow(ip.x_potential_minimum, 2)
+                     ) / 2.0
             virial_hamiltonian += (pot + kin)
             virial_hamiltonian2 += pow(pot + kin, 2)
             pot = 0.0
             kin = 0.0
-
 
     return virial_hamiltonian, virial_hamiltonian2
 
@@ -139,14 +138,13 @@ def free_energy_anharm(n_beta,
                        n_mc_sweeps,
                        n_switching,
                        i_cold):
-
     # Control output filepath
     output_path = './output_data/output_monte_carlo_switching'
     utility_custom.output_control(output_path)
 
     rnd.seed(time.time())
 
-    #Free Helmoltz energy for the anharmonic oscillator
+    # Free Helmoltz energy for the anharmonic oscillator
     beta_array = np.linspace(1.0, beta_max, n_beta, False)
     temperature_array = 1.0 / beta_array
 
@@ -154,15 +152,14 @@ def free_energy_anharm(n_beta,
     free_energy_err = np.empty((n_beta), float)
 
     for i_beta in range(n_beta):
-
-        n_lattice = int(beta_array[i_beta]/ip.dtau)
+        n_lattice = int(beta_array[i_beta] / ip.dtau)
 
         free_energy[i_beta], free_energy_err[i_beta] = \
-        monte_carlo_ao_switching(n_lattice,  # n_lattice
-                                  n_equil,  # n_equil
-                                  n_mc_sweeps,  # n_mc_sweeps-
-                                  n_switching,
-                                  i_cold)
+            monte_carlo_ao_switching(n_lattice,  # n_lattice
+                                     n_equil,  # n_equil
+                                     n_mc_sweeps,  # n_mc_sweeps-
+                                     n_switching,
+                                     i_cold)
         free_energy[i_beta] /= beta_array[i_beta]
         free_energy_err[i_beta] /= beta_array[i_beta]
         free_energy[i_beta] += free_energy_harmonic_osc(beta_array[i_beta])
@@ -186,6 +183,4 @@ def free_energy_anharm(n_beta,
         np.savetxt(f_writer, temperature_array)
         f_writer.write(str(1 / (n_virial * ip.dtau)))
 
-
-
-#cP.run("free_energy_anharm(4, 20.0, 100, 50000, 20, False)")
+# cP.run("free_energy_anharm(4, 20.0, 100, 50000, 20, False)")
