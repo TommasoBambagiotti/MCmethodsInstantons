f0bdcef96432f870f9a6b46ccbeebbc276880d8a
Federt1 <fedeale98.fa@gmail.com>
2022-11-09 10:28:58 +0100

    Update utility_custom.py

--- utility_custom.py
+++ utility_custom.py
@@ -34,7 +34,7 @@ def log_central_der_alg(corr_funct, corr_funct_err, delta_step):
         return None, None
 
     n_array = corr_funct.size
-    
+
     derivative_log = np.empty((n_array - 1), float)
     derivative_log_err = np.empty((n_array - 1), float)
 
@@ -72,7 +72,7 @@ def output_control(path_dir):
                 return 0
         else:
             parent_path.mkdir()
-            
+
     if path_output.exists() is True:
         if path_output.is_dir() is False:
             print('The path is not a directory: Error\n')
@@ -82,7 +82,7 @@ def output_control(path_dir):
         if (path_output.exists() is True) and (path_output.is_dir() is False):
             print('The path is not a directory: Error\n')
             return 0
-            
+
 # Monte carlo correlation functions
 
 def output_correlation_functions_and_log(n_points,
@@ -93,10 +93,10 @@ def output_correlation_functions_and_log(n_points,
 
     if type(x_cor_sums) is np.ndarray \
         and type(x2_cor_sums) is np.ndarray:
-        
+
         x_cor_av = np.zeros(x_cor_sums.shape)
         x_cor_err = np.zeros(x_cor_sums.shape)
-        
+
 
         for i_stat in range(3):
             x_cor_av[i_stat], x_cor_err[i_stat] = \
@@ -104,56 +104,56 @@ def output_correlation_functions_and_log(n_points,
                                x2_cor_sums[i_stat],
                                n_config
                                )
-            print(x_cor_av[i_stat])
+
             with open(output_path + f'/average_x_cor_{i_stat + 1}.txt', 'w',
                       encoding='utf8') as av_writer:
                 np.savetxt(av_writer, x_cor_av[i_stat])
-    
+
             with open(output_path + f'/error_x_cor_{i_stat + 1}.txt', 'w',
                       encoding='utf8') as err_writer:
                 np.savetxt(err_writer, x_cor_err[i_stat])
-    
+
         derivative_log_corr_funct = np.zeros((3, n_points - 1))
         derivative_log_corr_funct_err = np.zeros((3, n_points - 1))
-    
+
         for i_stat in range(3):
-    
+
             if i_stat != 1:
-    
+
                 derivative_log_corr_funct[i_stat], derivative_log_corr_funct_err[i_stat] = \
                     log_central_der_alg(
                         x_cor_av[i_stat], x_cor_err[i_stat], ip.dtau)
-    
+
             else:
                 # In the case of log <x^2x^2> the constant part <x^2>
                 # is circa the average for the greatest tau
-    
+
                 # subtraction of the constant term (<x^2>)^2 in <x(0)^2x(t)^2>
                 cor_funct_err = np.sqrt(np.square(x_cor_err[i_stat])
                                         + pow(x_cor_err[i_stat, -1], 2))
-    
+
                 derivative_log_corr_funct[i_stat], derivative_log_corr_funct_err[i_stat] = \
                     log_central_der_alg(
                         x_cor_av[i_stat] - x_cor_av[i_stat, -1],
                         cor_funct_err,
                         ip.dtau)
-    
+
                 # Save into files
-    
+
             # w/o cooling
             with open(output_path + f'/average_der_log_{i_stat + 1}.txt', 'w',
                       encoding='utf8') as av_writer:
                 np.savetxt(av_writer, derivative_log_corr_funct[i_stat])
-    
+
             with open(output_path + f'/error_der_log_{i_stat + 1}.txt', 'w',
                       encoding='utf8') as err_writer:
                 np.savetxt(err_writer, derivative_log_corr_funct_err[i_stat])
-    
+
         # time array
         with open(output_path + '/tau_array.txt', 'w',
                   encoding='utf8') as tau_writer:
             np.savetxt(tau_writer, np.linspace(
                 0, np.size(x_cor_sums, 1) * ip.dtau, np.size(x_cor_sums, 1), False))
-    
+
     else:
         return 0
