83e41ab8d434230ca292f95109a657e1a0ca4568
Federt1 <fedeale98.fa@gmail.com>
2022-12-08 16:54:04 +0100

    changes

--- graph_print.py
+++ graph_print.py
@@ -1,24 +1,79 @@
-import numpy as np
-import matplotlib.pyplot as plt
 import utility_custom
+import matplotlib.pyplot as plt
+import numpy as np
+import struct
+
+import matplotlib
+# matplotlib.use('pgf')
+# matplotlib.rcParams.update({
+#     "pgf.texsystem": "pdflatex",
+#     'font.family': 'serif',
+#     'font.sans-serif': ['Helvetica'],
+#     'text.usetex': True,
+#     'pgf.rcfonts': False,
+# })
+
+
+plt.style.use('ggplot')
 
 
 filepath = './output_graph'
 utility_custom.output_control(filepath)
 
 
+def print_ground_state():
+
+    filepath_loc = filepath + '/ground_state'
+    utility_custom.output_control(filepath_loc)
+    utility_custom.clear_folder(filepath_loc)    
+
+    fig1 = plt.figure(1,
+                      facecolor="#f1f1f1",
+                      figsize=(5, 5))
+    ax1 = fig1.add_axes((0.13, 0.1, 0.8, 0.8), facecolor="#e1e1e1")
+
+    ax1.set_title('Groundstate distribution $|\psi|^2$',
+                  )
+    ax1.set_xlabel(f'$x (arb. un.)$')
+    ax1.set_ylabel(f'$|\psi|^2$')
+
+    hist = np.loadtxt('./output_data/output_monte_carlo/ground_state_histogram.txt',
+                      float, delimiter=' ')
+
+    x_array = np.loadtxt('./output_data/output_diag/x_position_array.txt',
+                         float, delimiter=' ')
+    psi_ground_state = np.loadtxt('./output_data/output_diag/psi_ground_state.txt',
+                                  float, delimiter=' ')
+
+    psi_simple_model = np.loadtxt('./output_data/output_diag/psi_simple_model.txt',
+                                  float, delimiter=' ')
+
+    ax1.set_ylim(0.0, 0.55)
+    ax1.hist(hist, 100, (-3, 3), color='blue', label=f'Monte Carlo sim.',
+             density=True, histtype='step', linewidth=1)
+    ax1.plot(x_array, psi_simple_model, color='red',
+             label=f'$\psi$ simple model', linewidth=0.8)
+    ax1.plot(x_array, psi_ground_state, color='green',
+             label=f'$\psi$ ground state', linewidth=1)
+
+    ax1.legend(
+        fontsize=10)
+
+    fig1.savefig(filepath_loc + '/ground_state.png', dpi=300)
+
+    plt.show()
+
+
 def print_graph_free_energy():
-    """
 
-    """
-    plt.style.use('ggplot')
-    plt.rcParams.update({"text.usetex": True, "font.family": "Helvetica"})
+    fig1 = plt.figure(1,
+                      facecolor="#f1f1f1",
+                      figsize=(5, 5))
+    ax1 = fig1.add_axes((0.13, 0.1, 0.8, 0.8), facecolor="#e1e1e1")
 
-    fig1 = plt.figure(1, facecolor="#f1f1f1")
-    ax1 = fig1.add_axes((0.1, 0.1, 0.8, 0.8), facecolor="#e1e1e1")
-
-    ax1.set_xlabel(r"$T$")
-    ax1.set_ylabel(r"$F$")
+    ax1.set_title("Free energy F")
+    ax1.set_ylabel("F")
+    ax1.set_xlabel("Temperature")
 
     temperature_array = np.loadtxt('./output_data/output_monte_carlo_switching/temperature.txt',
                                    float, delimiter=' ')
@@ -39,7 +94,8 @@ def print_graph_free_energy():
                  color='b',
                  fmt='.',
                  capsize=2.5,
-                 elinewidth=0.5)
+                 elinewidth=0.5,
+                 label = 'Monte Carlo switching')
 
     ax1.errorbar(temperature_array,
                  vir,
@@ -48,18 +104,25 @@ def print_graph_free_energy():
                  marker='.',
                  color='red',
                  capsize=2.5,
-                 elinewidth=0.5)
-
-    ax1.set_xscale('log')
+                 elinewidth=0.5,
+                 label = 'Virial theorem')
 
     temperature_axis = np.loadtxt(
         './output_data/output_diag/temperature.txt', float, delimiter=' ')
     free_energy = np.loadtxt(
         './output_data/output_diag/free_energy.txt', float, delimiter=' ')
 
+
+
     ax1.plot(temperature_axis,
-             free_energy,
-             color='green')
+              free_energy,
+              color='green',
+              label = 'Free energy')
+
+    ax1.set_ylim(-2.3,-1.6)
+    ax1.legend(loc = 'upper left',
+         fontsize=10)
+    ax1.set_xscale('log')
 
     fig1.savefig(filepath + '/free_energy.png', dpi=300)
 
@@ -67,16 +130,8 @@ def print_graph_free_energy():
 
 
 def print_graph(folder):
-    """
 
-    Parameters
-    ----------
-    folder :
-    """
     utility_custom.output_control(filepath + '/' + folder)
-
-    plt.style.use('ggplot')
-    plt.rcParams.update({"text.usetex": True, "font.family": "Helvetica"})
 
     fig1 = plt.figure(1, facecolor="#f1f1f1")
 
@@ -241,14 +296,14 @@ def print_graph(folder):
 
 
 def print_density():
-    """
 
-    """
-    plt.style.use('ggplot')
-    plt.rcParams.update({"text.usetex": True, "font.family": "Helvetica"})
+    
+    fig = plt.figure(1, facecolor="#f1f1f1", figsize = (5,5))
+    ax = fig.add_axes((0.1, 0.1, 0.8, 0.8), facecolor="#e1e1e1")
 
-    fig3 = plt.figure(3, facecolor="#f1f1f1")
-    ax3 = fig3.add_axes((0.1, 0.1, 0.8, 0.8), facecolor="#e1e1e1")
+    ax.set_title('Instanton density')
+    ax.set_xlabel('$N_{cool}$')
+    ax.set_ylabel('$N_{total} \beta $')
 
     potential_minima = np.loadtxt(
         'output_data/output_cooled_monte_carlo/potential_minima.txt',
@@ -258,6 +313,8 @@ def print_density():
         './output_data/output_cooled_monte_carlo/n_cooling.txt',
         delimiter=' ')
 
+    
+    
     i = 1
     for pot in np.nditer(potential_minima):
         n_instantons = np.loadtxt(
@@ -265,13 +322,13 @@ def print_density():
         n_instantons_err = np.loadtxt(
             f'./output_data/output_cooled_monte_carlo/n_instantons_{i}_err.txt')
 
-        ax3.errorbar(n_cooling,
-                      n_instantons,
-                      n_instantons_err,
-                      fmt='.',
-                      capsize=2.5,
-                      elinewidth=0.5,
-                      label=f'$\eta = {pot}$')
+        ax.errorbar(n_cooling,
+                     n_instantons,
+                     n_instantons_err,
+                     fmt='.',
+                     capsize=2.5,
+                     elinewidth=0.5,
+                     label=f'$\eta = {pot}$')
 
         s0 = 4 / 3 * pow(pot, 3)
 
@@ -281,20 +338,25 @@ def print_density():
         loop_2 = 8 * pow(pot, 5 / 2) \
             * pow(2 / np.pi, 1/2) * np.exp(-s0 - 71 / (72 * s0))
 
-        ax3.hlines([loop_1, loop_2], 0, n_cooling[-1], color='green',
-                    linestyle=['dashed', 'solid'], linewidth=0.5)
+        ax.hlines([loop_1, loop_2], 0, n_cooling[-1], 
+                   linestyle=['dashed', 'solid'], linewidth=0.5)
 
         i += 1
 
-    ax3.legend()
-    ax3.set_xscale('log')
+    ax.legend()
+    ax.set_xscale('log')
 
-    ax3.set_yscale('log')
+    ax.set_yscale('log')
 
-    fig3.savefig(filepath + '/n_istantons.png', dpi=300)
+    # ax.ticklabel_format(axis = 'y', style = 'plain')
 
-    fig4 = plt.figure(4, facecolor="#f1f1f1")
-    ax4 = fig4.add_axes((0.1, 0.1, 0.8, 0.8), facecolor="#e1e1e1")
+    # current_values = ax.get_yticks()
+    # ax.set_yticklabels(['{:.2f}'.format(x) for x in current_values])
+
+    fig.savefig(filepath + '/n_istantons.png', dpi=300)
+
+    fig2 = plt.figure(2, facecolor="#f1f1f1", figsize = (5,5))
+    ax2 = fig2.add_axes((0.1, 0.1, 0.8, 0.8), facecolor="#e1e1e1")
 
     i = 1
     for pot in np.nditer(potential_minima):
@@ -306,7 +368,7 @@ def print_density():
             float,
             delimiter=' ')
 
-        ax4.errorbar(n_cooling,
+        ax2.errorbar(n_cooling,
                      action,
                      action_err,
                      fmt='.',
@@ -316,96 +378,41 @@ def print_density():
 
         s0 = 4 / 3 * pow(pot, 3)
 
-        ax4.hlines(s0, 0, n_cooling[-1], color='green',
+        ax2.hlines(s0, 0, n_cooling[-1], color='green',
                    linestyle='dashed', linewidth=0.8)
 
         i += 1
 
-    ax4.legend()
-    ax4.set_xscale('log')
-    ax4.set_yscale('log')
+    ax2.legend()
+    ax2.set_xscale('log')
+    ax2.set_yscale('log')
 
-    fig4.savefig(filepath + '/action.png', dpi=300)
+    # current_values = ax2.gca().get_yticks()
+    # ax2.gca().set_yticklabels([{'0.2f'}.format(x) for x in current_values])
 
-    plt.show()
+    fig2.savefig(filepath + '/action.png', dpi=300)
 
+    hot = np.loadtxt('./output_data/output_cooled_monte_carlo/hot.txt',
+                      delimiter=' ')
 
-def print_rilm_conf():
-    """
+    cold = np.loadtxt('./output_data/output_cooled_monte_carlo/cold.txt',
+                      delimiter=' ')
 
-    """
-    plt.style.use('ggplot')
-    plt.rcParams.update({"text.usetex": True, "font.family": "Helvetica"})
+    fig_3 = plt.figure(3, facecolor="#f1f1f1", figsize = (5,5))
+    ax_3 = fig_3.add_axes((0.1, 0.1, 0.8, 0.8), facecolor="#e1e1e1")
 
-    fig1 = plt.figure(1, facecolor="#f1f1f1")
+    ax_3.plot(np.linspace(0,hot.size,hot.size),
+              hot,
+              color = 'blue')
+    ax_3.plot(np.linspace(0,hot.size,hot.size),
+              cold,
+              color = 'red')
 
-    ax1 = fig1.add_axes((0.1, 0.1, 0.8, 0.8), facecolor="#e1e1e1")
-
-    conf = np.loadtxt('./output_data/output_rilm/conf_test.txt',
-                      float, delimiter=' ')
-
-    tau = np.loadtxt('./output_data/output_rilm/tau_test.txt',
-                     float, delimiter=' ')
-
-    ax1.plot(tau, conf)
-
-    plt.show()
-
-
-def print_graph_heat():
-    """
-
-    """
-    plt.style.use('ggplot')
-    plt.rcParams.update({"text.usetex": True, "font.family": "Helvetica"})
-
-    fig = plt.figure(1, facecolor="#f1f1f1")
-    ax = fig.add_axes((0.1, 0.1, 0.8, 0.8), facecolor="#e1e1e1")
-
-    tau_array = np.loadtxt('./output_data/output_rilm_heating/tau_array_conf.txt',
-                           float, delimiter=' ')
-    config = np.loadtxt('./output_data/output_rilm_heating/configuration.txt',
-                        float, delimiter=' ')
-    config_heat = np.loadtxt('./output_data/output_rilm_heating/configuration_heated.txt',
-                             float, delimiter=' ')
-
-    ax.plot(tau_array, config[0:-1], color='blue')
-    ax.plot(tau_array, config_heat[0:-1], color='red')
-
-    fig.savefig(filepath + '/heating.png', dpi=300)
-
-    plt.show()
-
-
-def print_stream():
-    """
-
-    """
-    plt.style.use('ggplot')
-    plt.rcParams.update({"text.usetex": True, "font.family": "Helvetica"})
-
-    fig1 = plt.figure(1, facecolor="#f1f1f1")
-
-    ax1 = fig1.add_axes((0.1, 0.1, 0.8, 0.8), facecolor="#e1e1e1")
-
-    tau = np.loadtxt('./output_data/output_iilm/streamline/tau_array.txt',
-                     float, delimiter=' ')
-
-    for i in range(3):
-        conf = np.loadtxt(f'./output_data/output_iilm/streamline/streamline_{i}.txt',
-                          float, delimiter=' ')
-        ax1.plot(tau, conf)
-        #print(mc.return_action(conf)/ip.action_0)
 
     plt.show()
 
 
 def print_zcr_hist():
-    """
-
-    """
-    plt.style.use('ggplot')
-    plt.rcParams.update({"text.usetex": True, "font.family": "Helvetica"})
 
     fig = plt.figure(1, facecolor="#f1f1f1")
     ax = fig.add_axes((0.1, 0.1, 0.8, 0.8), facecolor="#e1e1e1")
@@ -417,16 +424,20 @@ def print_zcr_hist():
                              float, delimiter=' ')
 
     zcr_int = np.loadtxt('./output_data/output_iilm/iilm/zcr_hist.txt',
-                             float, delimiter=' ')
-    
+                         float, delimiter=' ')
+
     print(zcr_int.size)
     print(zcr.size)
     print(zcr_cooling.size)
 
-    ax.hist(zcr, 39, (0.1, 4.), histtype='step', color = 'red')
-    ax.hist(zcr_int, 39, (0.1, 4.), histtype='step', color = 'orange')#, density='True')
-    ax.hist(zcr_cooling, 39, (0.1, 4.), histtype='step',
-            color='blue')#, density='True')
+    ax.hist(zcr, 39, (0.1, 4.), histtype='step',
+            color='red', label='random liquid model')
+    ax.hist(zcr_int, 39, (0.1, 4.), histtype='step', color='orange',
+            label='core repulsion')  # , density='True')
+    ax.hist(zcr_cooling, 39, (0.1, 4.), histtype='step', label='monte carlo cooling',
+            color='blue')  # , density='True')
+
+    ax.legend()
 
     fig.savefig(filepath + '/zcr_histogram.png', dpi=300)
 
@@ -434,11 +445,6 @@ def print_zcr_hist():
 
 
 def print_tau_centers():
-    """
-
-    """
-    plt.style.use('ggplot')
-    plt.rcParams.update({"text.usetex": True, "font.family": "Helvetica"})
 
     fig = plt.figure(1, facecolor="#f1f1f1")
     ax = fig.add_axes((0.1, 0.1, 0.8, 0.8), facecolor="#e1e1e1")
@@ -462,11 +468,6 @@ def print_tau_centers():
 
 
 def print_cool_density():
-    """
-
-    """
-    plt.style.use('ggplot')
-    plt.rcParams.update({"text.usetex": True, "font.family": "Helvetica"})
 
     fig = plt.figure(1, facecolor="#f1f1f1")
     ax = fig.add_axes((0.1, 0.1, 0.8, 0.8), facecolor="#e1e1e1")
@@ -497,15 +498,16 @@ def print_cool_density():
                 j += 1
 
         i += 1
-    ax.errorbar(potential_minima, 
-                dens, 
-                dens_err, 
-                color='blue', 
+    ax.errorbar(potential_minima,
+                dens,
+                dens_err,
+                color='blue',
                 marker='s',
                 linestyle='',
                 capsize=2.5,
                 elinewidth=0.5,
-                label = 'cooling')
+                markersize=4.,
+                label='cooling')
 
     potential = np.linspace(0.1, 2.0)
     action = 4/3 * np.power(potential, 3)
@@ -516,19 +518,18 @@ def print_cool_density():
     loop_2 = loop_1 * np.exp(-71/72/action)
 
     ax.plot(potential, loop_1,
-            linestyle='--', color='green', label = '1-loop')
+            linestyle='--', color='green', label='1-loop')
 
     ax.plot(potential, loop_2,
-            linestyle='-', color='green', label = '2-loop')
-
+            linestyle='-', color='green', label='2-loop')
 
     potential_minima = np.loadtxt(
         'output_data/output_monte_carlo_density_switching/potential_minima.txt',
-                                  delimiter=' ')
-    
+        delimiter=' ')
+
     dens = np.loadtxt(
         'output_data/output_monte_carlo_density_switching/total_density.txt')
-    
+
     dens_err = np.loadtxt(
         'output_data/output_monte_carlo_density_switching/total_density_err.txt')
 
@@ -536,108 +537,103 @@ def print_cool_density():
         potential_minima,
         dens,
         dens_err,
-        color = 'red',
+        color='red',
         marker='s',
         linestyle='',
         capsize=2.5,
         elinewidth=0.5,
-        label = 'Monte Carlo')
+        markersize=4.,
+        label='Monte Carlo')
 
     ax.legend()
-    ax.set_xlim(0.0, 1.75)
+    ax.set_xlim(0.0, 1.9)
     ax.set_ylim(0.03, 2.)
 
     ax.set_yscale('log')
 
-    fig.savefig(filepath + '/density.png', dpi = 300)
+    fig.savefig(filepath + '/density.png', dpi=300)
 
     plt.show()
 
-def print_iilm():
-    """
 
-    """
-    plt.style.use('ggplot')
-    plt.rcParams.update({"text.usetex": True, "font.family": "Helvetica"})
+def print_iilm():
 
     fig1 = plt.figure(1, facecolor="#f1f1f1")
 
     ax1 = fig1.add_axes((0.1, 0.1, 0.8, 0.8), facecolor="#e1e1e1")
-    
+
     tau_ia = np.loadtxt('./output_data/output_iilm/streamline/delta_tau_ia.txt',
-                           float, delimiter=' ')
-    
+                        float, delimiter=' ')
+
     zcr = np.loadtxt('./output_data/output_rilm/zcr_hist.txt', float,
                      delimiter=' ')
     zcr_cooling = np.loadtxt('./output_data/output_cooled_monte_carlo/zero_crossing/zcr_cooling.txt',
                              float, delimiter=' ')
-    
-    hist_1, _ = np.histogram(zcr, 40, range =(0.,4.))
-    hist_2, _ = np.histogram(zcr_cooling, 40, range =(0.,4.))
-    
+
+    hist_1, _ = np.histogram(zcr, 40, range=(0., 4.))
+    hist_2, _ = np.histogram(zcr_cooling, 40, range=(0., 4.))
+
     # bisogna implementare un modo per capire già quanto vale il potentiale?
-    # tipo costruire l'istogramma già nella funzione zero_... 
-    action_ia = -np.log(hist_2[0:10]/hist_1[0:10])/(4/3*np.power(1.4,3))
-    
-    
+    # tipo costruire l'istogramma già nella funzione zero_...
+    action_ia = -np.log(hist_2[0:10]/hist_1[0:10])/(4/3*np.power(1.4, 3))
+
     act_int = np.loadtxt('./output_data/output_iilm/streamline/streamline_action_int.txt',
-                            float, delimiter = ' ')
-    
+                         float, delimiter=' ')
+
     array_ia = np.loadtxt('./output_data/output_iilm/streamline/array_ia.txt',
-                           float, delimiter=' ')
-    
+                          float, delimiter=' ')
+
     array_ia_0 = np.loadtxt('./output_data/output_iilm/streamline/array_ia_0.txt',
-                           float, delimiter=' ')
-    
+                            float, delimiter=' ')
+
     array_int = np.loadtxt('./output_data/output_iilm/streamline/array_int.txt',
-                            float, delimiter = ' ')
-    
+                           float, delimiter=' ')
+
     array_int_core = np.loadtxt('./output_data/output_iilm/streamline/array_int_core.txt',
-                            float, delimiter = ' ')
-    
+                                float, delimiter=' ')
+
     array_int_zero_cross = np.loadtxt('./output_data/output_iilm/streamline/array_int_zero_cross.txt',
-                            float, delimiter = ' ')
-    
-    
-    ax1.plot(tau_ia, 
+                                      float, delimiter=' ')
+
+    ax1.plot(tau_ia,
              act_int,
-             marker = '^',
-             linestyle ='',
-             color = 'green',
-             label = 'streamline'
+             marker='^',
+             linestyle='',
+             color='green',
+             label='streamline'
              )
-    
+
     ax1.plot(array_ia,
              array_int,
-             color = 'blue',
-             label = 'sum ansatz')
-    
+             color='blue',
+             label='sum ansatz')
+
     ax1.plot(array_ia,
              array_int_core,
-             color = 'orange',
-             label = 'core repulsion'
+             color='orange',
+             label='core repulsion'
              )
-    
+
     ax1.plot(array_ia_0,
              array_int_zero_cross,
-             color = 'red',
-             marker = 's',
-             linestyle = '',
-             label = 'zcr sum ansatz'
+             color='red',
+             marker='s',
+             linestyle='',
+             label='zcr sum ansatz'
              )
-    
+
     ax1.plot(np.linspace(0.1, 1.0, 10, False),
              action_ia,
-             marker = 's',
-             linestyle = '',
-             color = 'cyan',
-             label = 'monte carlo cooling')
-    
+             marker='s',
+             linestyle='',
+             color='cyan',
+             label='monte carlo cooling')
+
     ax1.legend()
-    
-    ax1.set_ylim(-2.0,0.5)
-    ax1.set_xlim(-0.05, 2.0)
-    
-    fig1.savefig(filepath + '/iilm.png', dpi = 300)
-    
+
+    ax1.set_ylim(-2.0, 0.5)
+    ax1.set_xlim(-0.05, 2.3)
+
+    fig1.savefig(filepath + '/iilm.png', dpi=300)
+
     plt.show()
